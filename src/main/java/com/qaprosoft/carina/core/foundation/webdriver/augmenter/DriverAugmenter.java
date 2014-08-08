662aba07b211e1f2b1a3da3bfcf6edaf2c6eb8f0
vdelendik <vadim.delendik@gmail.com>
2014-08-09 02:26:36 +0300

    added DriverAugmenter to support screenshoting without warnings

---
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/augmenter/DriverAugmenter.java
@@ -0,0 +1,32 @@
+package com.qaprosoft.carina.core.foundation.webdriver.augmenter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.Augmentable;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class DriverAugmenter extends Augmenter {
+
+	public DriverAugmenter() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected RemoteWebDriver extractRemoteWebDriver(WebDriver driver) {
+		if (driver.getClass().isAnnotationPresent(Augmentable.class)
+				|| driver
+						.getClass()
+						.getName()
+						.startsWith(
+								"org.openqa.selenium.remote.RemoteWebDriver$$EnhancerByCGLIB")
+				|| driver
+						.getClass()
+						.getName()
+						.startsWith(
+								"com.qaprosoft.carina.core.foundation.webdriver")) {
+			return (RemoteWebDriver) driver;
+		} else {
+			return null;
+		}
+	}
+}
