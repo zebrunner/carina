a1c76d8a009ff8e5df8bcb80d952a00414f40918
vdelendik <vadim.delendik@gmail.com>
2014-11-17 22:36:45 +0300

    rolleb back unreleased resource fixes

--- src/main/java/com/qaprosoft/carina/core/foundation/utils/android/recorder/utils/Platform.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/utils/android/recorder/utils/Platform.java
@@ -1,41 +1,28 @@
 package com.qaprosoft.carina.core.foundation.utils.android.recorder.utils;
 
+import com.sun.jna.Pointer;
+import org.apache.log4j.Logger;
+
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Locale;
-
-import org.apache.log4j.Logger;
-
-import com.sun.jna.Pointer;
 
 /**
  *
  */
 public class Platform {
 
-	private static final Logger LOGGER = Logger.getLogger(Platform.class);
+    private static final Logger LOGGER = Logger.getLogger(Platform.class);
 
-	public static final String OS_NAME_KEY = "os.name";
-	public static String name;
-	
-	public static final boolean IS_WINDOWS = name.startsWith("windows");
-	public static final boolean IS_MAC_OS_X = name.startsWith("mac os x");
-	public static final boolean IS_LINUX = name.startsWith("linux");
+	public static final String NAME = System.getProperty("os.name").toLowerCase(Locale.US);
+	public static final boolean IS_WINDOWS = NAME.startsWith("windows");
+	public static final boolean IS_MAC_OS_X = NAME.startsWith("mac os x");
+	public static final boolean IS_LINUX = NAME.startsWith("linux");
 
 	private static final String[] WIN_CMD = { };
 	private static final String[] MAC_CMD = { };
 //	private static final String[] WIN_CMD = { "cmd", "/c" };
 //	private static final String[] MAC_CMD = { "/bin/bash", "-c" };
-	
-	static
-	{
-		String osName = System.getProperty(OS_NAME_KEY);
-		if (osName == null)
-		{
-			throw new RuntimeException(OS_NAME_KEY + " system property not specified");
-		}
-		name = osName.toLowerCase(Locale.US);
-	}
 
 	public static String[] getCmd() {
 		if (Platform.IS_WINDOWS) {
@@ -75,14 +62,9 @@ public class Platform {
 			//String cmd = sb.append(" /f /t").toString();
             String cmd = sb.append(" /F /T").toString();
 			LOGGER.debug(cmd);
-			Process p = Runtime.getRuntime().exec(cmd);
-			int processStatus = p.waitFor();
-			if(processStatus != 0)
-			{
-				throw new RuntimeException("Process not terminated properly. Code: " + processStatus);
-			}
+			Runtime.getRuntime().exec(cmd);
 		} catch (Exception e) {
-			LOGGER.error(e);
+			// ignore
 		}
 	}
 
@@ -94,16 +76,10 @@ public class Platform {
             }
             String cmd = sb.toString();
             LOGGER.debug(cmd);
-			Process p = Runtime.getRuntime().exec(cmd);
-			int processStatus = p.waitFor();
-			if (processStatus != 0)
-			{
-				throw new RuntimeException("Process not terminated properly. Code: " + processStatus);
-			}
-		} catch (Exception e)
-		{
-			LOGGER.error(e);
-		}
+            Runtime.getRuntime().exec(cmd);
+        }catch (Exception e){
+            //ignore
+        }
     }
 
 	private static int getUnixProcessPID(Process process) {
