277ca604291878e91b84ff741f8c6f23883a1b81
vdelendik <vadim.delendik@gmail.com>
2014-12-16 21:04:40 +0300

    AUTO-731 jobs with several test classes are not registered in zafira reporting service

--- src/main/java/com/qaprosoft/carina/core/foundation/report/zafira/ZafiraIntegrator.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/report/zafira/ZafiraIntegrator.java
@@ -60,7 +60,7 @@ public class ZafiraIntegrator {
 	private static final ZafiraClient zc = new ZafiraClient(zafiraUrl);
 	
 	public static void startSuite(ITestContext context) {
-		if (!isValid())
+		if (!isValid() && !isRegistered)
 			return;
 		
 		try {
