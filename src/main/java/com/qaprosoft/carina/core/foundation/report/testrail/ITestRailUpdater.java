caabea04568ff4ae94d1c96bdd3a9c23e98e19ba
vdelendik <vadim.delendik@gmail.com>
2015-01-19 18:39:46 +0300

    TestRail small improvement

--- src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/ITestRailUpdater.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/ITestRailUpdater.java
@@ -4,7 +4,7 @@ import org.testng.ITestResult;
 
 
 public interface ITestRailUpdater {
-	public void updateBeforeSuite(String testClass);
+	public void updateBeforeSuite(String testClass, String title);
 	
 	public void updateAfterTest(ITestResult result, String errorMessage);
 }
