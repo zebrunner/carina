98134128b68026f606f702b79766e56ebaac3db0
vdelendik <vadim.delendik@gmail.com>
2015-04-07 19:21:09 +0300

    testrails improvements

--- src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/dto/Results.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/dto/Results.java
@@ -7,22 +7,27 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 import com.qaprosoft.carina.core.foundation.report.testrail.core.Request;
-import com.qaprosoft.carina.core.foundation.report.testrail.core.TestStatus;
 
 public class Results {
 
-    public static Request addResults(int runId, HashMap<String,TestStatus> testStatusHashMap) {
-        JSONArray jsonArray = new JSONArray();
-        for (Map.Entry<String,TestStatus> entry : testStatusHashMap.entrySet()) {
-            JSONObject obj=new JSONObject();
-            obj.put("case_id", Integer.parseInt(entry.getKey()));
-            obj.put("status_id", entry.getValue().getNumber());
+	@SuppressWarnings("unchecked")
+	public static Request addResults(int runId, HashMap<String, TestCaseResult> testCaseResultHashMap) {
+	        JSONArray jsonArray = new JSONArray();
+	        for (Map.Entry<String, TestCaseResult> entry : testCaseResultHashMap.entrySet()) {
+	            JSONObject obj = new JSONObject();
+	            obj.put("case_id", Integer.parseInt(entry.getKey()));
+	            obj.put("status_id", entry.getValue().getTestStatus().getNumber());
+	            obj.put("version", entry.getValue().getVersion());
+	            obj.put("assignedto_id", entry.getValue().getAssignTo());
+	            obj.put("comment", entry.getValue().getComment());
+	            obj.put("defects", entry.getValue().getDefects());
 
-            jsonArray.add(obj);
-        }
-        JSONObject finalObject = new JSONObject();
-        finalObject.put("results", jsonArray);
 
-        return new Request(finalObject,"add_results_for_cases/"+runId,"POST");
-    }
+	            jsonArray.add(obj);
+	        }
+	        JSONObject finalObject = new JSONObject();
+	        finalObject.put("results", jsonArray);
+
+	        return new Request(finalObject, "add_results_for_cases/" + runId, "POST");
+	    }
 }
