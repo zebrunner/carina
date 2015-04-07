b5975ad4e43ab798f49a191053fa574997384246
patotskiy <hathi.blr@gmail.com>
2015-04-07 19:47:57 +0300

    Add possibility to publish links on jenkins reports

--- src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/dto/Results.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/dto/Results.java
@@ -1,13 +1,12 @@
 package com.qaprosoft.carina.core.foundation.report.testrail.dto;
 
-import java.util.HashMap;
-import java.util.Map;
-
+import com.qaprosoft.carina.core.foundation.report.testrail.core.Request;
+import com.qaprosoft.carina.core.foundation.report.testrail.core.TestStatus;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
-import com.qaprosoft.carina.core.foundation.report.testrail.core.Request;
-import com.qaprosoft.carina.core.foundation.report.testrail.core.TestStatus;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Results {
 
@@ -24,5 +23,25 @@ public class Results {
         finalObject.put("results", jsonArray);
 
         return new Request(finalObject,"add_results_for_cases/"+runId,"POST");
+    }
+
+    public static Request addResultsWithLinks(int runId, Map<String, TestCaseResult> testCaseResultHashMap) {
+        JSONArray jsonArray = new JSONArray();
+        for (Map.Entry<String, TestCaseResult> entry : testCaseResultHashMap.entrySet()) {
+            JSONObject obj = new JSONObject();
+            obj.put("case_id", Integer.parseInt(entry.getKey()));
+            obj.put("status_id", entry.getValue().getTestStatus().getNumber());
+            obj.put("version", entry.getValue().getVersion());
+            obj.put("assignedto_id", entry.getValue().getAssignTo());
+            obj.put("comment", entry.getValue().getComment());
+            obj.put("defects", entry.getValue().getDefects());
+
+
+            jsonArray.add(obj);
+        }
+        JSONObject finalObject = new JSONObject();
+        finalObject.put("results", jsonArray);
+
+        return new Request(finalObject, "add_results_for_cases/" + runId, "POST");
     }
 }
