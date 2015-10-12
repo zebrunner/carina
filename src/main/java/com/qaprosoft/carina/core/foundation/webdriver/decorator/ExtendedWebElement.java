1ab5d4c9e705750ecdb8d0359278b17a0fa6c2b4
Sergei Zagriychuk <serge@onflydev.com>
2015-10-13 00:42:19 +0300

    -moving to parameter scrollTo offset; -html report updates

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/decorator/ExtendedWebElement.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/decorator/ExtendedWebElement.java
@@ -21,6 +21,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.regex.Pattern;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.log4j.Logger;
 import org.hamcrest.BaseMatcher;
 import org.openqa.selenium.By;
@@ -43,6 +44,7 @@ import com.qaprosoft.carina.core.foundation.log.TestLogHelper;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 import com.qaprosoft.carina.core.foundation.utils.Messager;
+import com.qaprosoft.carina.core.foundation.utils.R;
 import com.qaprosoft.carina.core.foundation.utils.SpecialKeywords;
 import com.qaprosoft.carina.core.foundation.webdriver.DriverPool;
 import com.qaprosoft.carina.core.foundation.webdriver.Screenshot;
@@ -635,7 +637,11 @@ public class ExtendedWebElement
 			//[CB] onPage -> inViewPort
 			//https://code.google.com/p/selenium/source/browse/java/client/src/org/openqa/selenium/remote/RemoteWebElement.java?r=abc64b1df10d5f5d72d11fba37fabf5e85644081
 			int y = locatableElement.getCoordinates().inViewPort().getY();
-			((JavascriptExecutor) getDriver()).executeScript("window.scrollBy(0," + (y - 120) + ");");
+			if (!StringUtils.EMPTY.equals(R.CONFIG.get("scroll_to_element_y_offset")))
+			{
+				((JavascriptExecutor) getDriver()).executeScript("window.scrollBy(0,"
+						+ (y - R.CONFIG.getInt("scroll_to_element_y_offset")) + ");");
+			}
 		}
 		catch (Exception e)
 		{
