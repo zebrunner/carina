a708a2beed1a32cf4829de7a8a5c6bc7a28c4020
vdelendik <vadim.delendik@gmail.com>
2015-11-15 20:05:36 +0300

    added cdata_provider_thread_count as condition to apk copying before multi-threading Android tests startup

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/DriverFactory.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/DriverFactory.java
@@ -159,7 +159,10 @@ public class DriverFactory {
                     
                     String mobileAppPath = Configuration.get(Parameter.MOBILE_APP);
                     
-                    if (Configuration.getInt(Parameter.THREAD_COUNT) > 1 && Configuration.get(Parameter.MOBILE_PLATFORM_NAME).equalsIgnoreCase(SpecialKeywords.ANDROID)) {
+					if ((Configuration.getInt(Parameter.THREAD_COUNT) > 1
+							|| Configuration.getInt(Parameter.DATA_PROVIDER_THREAD_COUNT) > 1)
+							&& Configuration.get(Parameter.MOBILE_PLATFORM_NAME)
+									.equalsIgnoreCase(SpecialKeywords.ANDROID)) {
 	                    //[VD] workaround to the issue with multiply calls to the single apk files
 	                    File mobileAppFile = new File(Configuration.get(Parameter.MOBILE_APP));
 	                    File appTempFile = new File(ReportContext.getTempDir().getAbsolutePath() + File.separator + device.getUdid() + "-" + mobileAppFile.getName());
