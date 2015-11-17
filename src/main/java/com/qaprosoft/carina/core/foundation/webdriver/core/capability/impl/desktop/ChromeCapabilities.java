8e984f0675f4cd08720c960125debdc9ecc3fd30
vdelendik <vadim.delendik@gmail.com>
2015-11-17 22:37:45 +0300

    reviewing abstract getcapabilities and minimized required parameters

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/desktop/ChromeCapabilities.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/desktop/ChromeCapabilities.java
@@ -1,19 +1,20 @@
 package com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop;
 
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.AbstractCapabilities;
+import java.util.Arrays;
+
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
-import java.util.Arrays;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.AbstractCapabilities;
 
 public class ChromeCapabilities extends AbstractCapabilities {
 
 
-    public DesiredCapabilities getCapability(String browserVersion, String testName) {
+    public DesiredCapabilities getCapability(String testName) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities = initBaseCapabilities(capabilities, BrowserType.CHROME, browserVersion, testName);
+        capabilities = initBaseCapabilities(capabilities, BrowserType.CHROME, testName);
         capabilities.setCapability("chrome.switches", Arrays.asList("--start-maximized"));
         capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
         capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, false);
