47f0782c68ee1091565421c8d53bcb3ec932e14b
vdelendik <vadim.delendik@gmail.com>
2015-11-17 21:43:08 +0300

    removed mobile_browser_name property and reused browser instead...

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/mobile/MobileWebCapabilities.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/mobile/MobileWebCapabilities.java
@@ -14,7 +14,7 @@ public class MobileWebCapabilities extends MobileCapabilies {
         DesiredCapabilities capabilities = getMobileCapabilities(isGrid, Configuration.get(Configuration.Parameter.MOBILE_PLATFORM_NAME),
                 Configuration.get(Configuration.Parameter.MOBILE_PLATFORM_VERSION), Configuration.get(Configuration.Parameter.MOBILE_DEVICE_NAME),
                 Configuration.get(Configuration.Parameter.MOBILE_AUTOMATION_NAME), Configuration.get(Configuration.Parameter.MOBILE_NEW_COMMAND_TIMEOUT),
-                Configuration.get(Configuration.Parameter.MOBILE_BROWSER_NAME), null, null, null);
+                Configuration.get(Configuration.Parameter.BROWSER), null, null, null);
         return capabilities;
     }
 }
