5b839c2e44bc38844b64662c0abfe1731f8795fa
vdelendik <vadim.delendik@gmail.com>
2015-12-03 19:12:52 +0300

    improved custom capabilities loader

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/DesktopFactory.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/DesktopFactory.java
@@ -1,21 +1,25 @@
 package com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl;
 
-import com.qaprosoft.carina.core.foundation.utils.Configuration;
-import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
-import com.qaprosoft.carina.core.foundation.utils.SpecialKeywords;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.CapabilitiesLoder;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.*;
-import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
-import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.qaprosoft.carina.core.foundation.utils.Configuration;
+import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.CapabilitiesLoder;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.ChromeCapabilities;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.FirefoxCapabilities;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.HTMLUnitCapabilities;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.IECapabilities;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.SafariCapabilities;
+import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
+import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
 
 public class DesktopFactory extends AbstractFactory {
 
@@ -35,11 +39,8 @@ public class DesktopFactory extends AbstractFactory {
 
 
     public DesiredCapabilities getCapabilities(String testName) {
-    	String driverType = Configuration.get(Parameter.DRIVER_TYPE);
-    	String browser = Configuration.get(Parameter.BROWSER);
-    	
-    	
-		if (driverType.equalsIgnoreCase(SpecialKeywords.CUSTOM)) {
+    	String customCapabilities = Configuration.get(Parameter.CUSTOM_CAPABILITIES);
+		if (!customCapabilities.isEmpty()) {
 			try {
 				return new CapabilitiesLoder(new FileInputStream(Configuration.get(Parameter.CUSTOM_CAPABILITIES)))
 						.loadCapabilities();
@@ -47,7 +48,10 @@ public class DesktopFactory extends AbstractFactory {
 				throw new RuntimeException(
 						"Unable read custom capabilities: " + Configuration.get(Parameter.CUSTOM_CAPABILITIES));
 			}
-		} else if (driverType.equalsIgnoreCase(SpecialKeywords.DESKTOP)) {
+		} 
+		else {
+			String browser = Configuration.get(Parameter.BROWSER);
+			
 			if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
 				return new FirefoxCapabilities().getCapability(testName);
 			} else if (BrowserType.IEXPLORE.equalsIgnoreCase(browser) || BrowserType.IE.equalsIgnoreCase(browser)
@@ -62,8 +66,6 @@ public class DesktopFactory extends AbstractFactory {
 			} else {
 				throw new RuntimeException("Unsupported browser: " + browser);
 			}
-		} else {
-			throw new RuntimeException("Unsupported driverType: " + driverType);
 		}
 
     }
