c873c431da869879ed330e87b1bee5a0924f8fc0
vdelendik <vadim.delendik@gmail.com>
2016-01-22 01:06:39 +0300

    AUTO-1867: NPE fix for DataProvider

--- src/main/java/com/qaprosoft/carina/core/foundation/dataprovider/core/impl/XlsDataProvider.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/dataprovider/core/impl/XlsDataProvider.java
@@ -157,9 +157,11 @@ public class XlsDataProvider extends BaseDataProvider {
     private void addValueToSpecialMap(Map<String,String> map, String column, String hashCode, Map<String,String> xlsRow) {
         if (column != null) {
             if (!column.isEmpty()) {
-            	if (!xlsRow.get(column).isEmpty()) {
-            		//put into the args only non empty jira tickets
-            		map.put(hashCode, xlsRow.get(column));
+            	if (xlsRow.get(column) != null) {
+	            	if (!xlsRow.get(column).isEmpty()) {
+	            		//put into the args only non empty jira tickets
+	            		map.put(hashCode, xlsRow.get(column));
+	            	}
             	}
             }
         }    	
