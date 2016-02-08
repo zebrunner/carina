c3e1c83278cbc26313b0c3e4ace92bf875dd78d0
vdelendik <vadim.delendik@gmail.com>
2016-02-08 16:10:26 +0300

    added chrome auto download settings like for FF

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/desktop/ChromeCapabilities.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/desktop/ChromeCapabilities.java
@@ -1,12 +1,14 @@
 package com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import com.qaprosoft.carina.core.foundation.report.ReportContext;
 import com.qaprosoft.carina.core.foundation.webdriver.core.capability.AbstractCapabilities;
 
 public class ChromeCapabilities extends AbstractCapabilities {
@@ -20,6 +22,14 @@ public class ChromeCapabilities extends AbstractCapabilities {
         capabilities.setCapability(CapabilityType.TAKES_SCREENSHOT, false);
         ChromeOptions options = new ChromeOptions();
         options.addArguments("test-type");
+        
+        HashMap<String, Object> chromePrefs = new HashMap<String, Object>();
+        chromePrefs.put("profile.default_content_settings.popups", 0);
+        chromePrefs.put("download.default_directory", ReportContext.getArtifactsFolder().getAbsolutePath());
+        chromePrefs.put("safebrowsing.enabled", "true");
+        
+        options.setExperimentalOption("prefs", chromePrefs);
+        
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
         return capabilities;
     }
