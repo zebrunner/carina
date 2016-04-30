b662c4c86a659f0cc4ab297afd186ea7d9e8ab22
vdelendik <vadim.delendik@gmail.com>
2016-04-29 17:04:43 -0700

    refactored proxy usage to use proxy_host and proxy_port parameters

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/AbstractCapabilities.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/AbstractCapabilities.java
@@ -1,19 +1,14 @@
 package com.qaprosoft.carina.core.foundation.webdriver.core.capability;
 
-import java.util.regex.Pattern;
-
 import org.apache.log4j.Logger;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.Assert;
 
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 
 public abstract class AbstractCapabilities {
-	private static final String PROXY_PATTERN = ".+:.+:.+";
-
     protected static final Logger LOGGER = Logger.getLogger(AbstractCapabilities.class);
 
     public abstract DesiredCapabilities getCapability(String testName);
@@ -31,37 +26,30 @@ public abstract class AbstractCapabilities {
         capabilities.setCapability("name", testName);
         
         
-		if (!Configuration.isNull(Parameter.PROXY) && !Configuration.get(Parameter.PROXY).isEmpty()) {
-			String PROXY = Configuration.get(Parameter.PROXY);
-			if (Pattern.matches(PROXY_PATTERN, Configuration.get(Parameter.PROXY))) {
+		String proxyHost = Configuration.get(Parameter.PROXY_HOST);
+		String proxyPort = Configuration.get(Parameter.PROXY_PORT);
+		
+		if (proxyHost != null && !proxyHost.isEmpty() && proxyPort != null && !proxyPort.isEmpty()) {
+			System.setProperty("http.proxyHost", proxyHost);
+			System.setProperty("http.proxyPort", proxyPort);
 
-				String[] proxies = Configuration.get(Parameter.PROXY).split(":");
-				System.setProperty(String.format("%s.proxyHost", proxies[0]), proxies[1]);
-				System.setProperty(String.format("%s.proxyPort", proxies[0]), proxies[2]);
-				LOGGER.info(
-						String.format("WebDriver will use proxy: %s://%s:%s", proxies[0], proxies[1], proxies[2]));
+			System.setProperty("https.proxyHost", proxyHost);
+			System.setProperty("https.proxyPort", proxyPort);
 
-				org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
-				switch (proxies[0]) {
-				case "http":
-					proxy.setHttpProxy(String.format("%s:%s", proxies[1], proxies[2]));
-					break;
-				case "https":
-					proxy.setSslProxy(String.format("%s:%s", proxies[1], proxies[2]));
-					break;
-				case "ftp":
-					proxy.setFtpProxy(String.format("%s:%s", proxies[1], proxies[2]));
-					break;
-				default:
-					Assert.fail("Unsupported proxy protocol specified: " + proxies[0]);
-				}
-				// proxy.setHttpProxy(PROXY).setFtpProxy(PROXY).setSslProxy(PROXY);
-				capabilities.setCapability(CapabilityType.PROXY, proxy);
-			} else {
-				Assert.fail("Invalid proxy is specified: " + PROXY);
-			}
+			System.setProperty("ftp.proxyHost", proxyHost);
+			System.setProperty("ftp.proxyPort", proxyPort);
+			
+			org.openqa.selenium.Proxy proxy = new org.openqa.selenium.Proxy();
+			String proxyAddress = String.format("%s:%s", proxyHost, proxyPort);
+			proxy.setHttpProxy(proxyAddress);
+			proxy.setSslProxy(proxyAddress);
+			proxy.setFtpProxy(proxyAddress);
+			capabilities.setCapability(CapabilityType.PROXY, proxy);
+
+
+			LOGGER.info(String.format("WebDriver will use http/https/ftp proxies: %s:%s", proxyHost, proxyPort));
 		}
-        
+		
         return capabilities;
     }
     
