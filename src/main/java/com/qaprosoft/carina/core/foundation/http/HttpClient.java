ee48908cade6f46560de1a776a7948653b0c5f5a
vdelendik <vadim.delendik@gmail.com>
2016-08-11 18:33:41 -0700

    AUTO-2337: split proxy support by protocols

--- src/main/java/com/qaprosoft/carina/core/foundation/http/HttpClient.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/http/HttpClient.java
@@ -16,6 +16,9 @@
 package com.qaprosoft.carina.core.foundation.http;
 
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 
 import com.jayway.restassured.response.Response;
@@ -68,20 +71,35 @@ public class HttpClient
 		String proxyHost = Configuration.get(Parameter.PROXY_HOST);
 		String proxyPort = Configuration.get(Parameter.PROXY_PORT);
 		
+		List<String> protocols = Arrays.asList(Configuration.get(Parameter.PROXY_PROTOCOLS).split("[\\s,]+"));
+		
 		if (proxyHost != null && !proxyHost.isEmpty() && proxyPort != null && !proxyPort.isEmpty()) {
-			System.setProperty("http.proxyHost", proxyHost);
-			System.setProperty("http.proxyPort", proxyPort);
+			if (protocols.contains("http")) {
+				LOGGER.info(String.format("HTTP client will use http: %s:%s", proxyHost, proxyPort));
 
-			System.setProperty("https.proxyHost", proxyHost);
-			System.setProperty("https.proxyPort", proxyPort);
+				System.setProperty("http.proxyHost", proxyHost);
+				System.setProperty("http.proxyPort", proxyPort);
+			}
 
-			System.setProperty("ftp.proxyHost", proxyHost);
-			System.setProperty("ftp.proxyPort", proxyPort);
-			
-			System.setProperty("socksProxyHost", proxyHost);
-			System.setProperty("socksProxyPort", proxyPort);
+			if (protocols.contains("https")) {
+				LOGGER.info(String.format("HTTP client will use https proxies: %s:%s", proxyHost, proxyPort));
 
-			LOGGER.info(String.format("HTTP client will use http/https/ftp proxies: %s:%s", proxyHost, proxyPort));
+				System.setProperty("https.proxyHost", proxyHost);
+				System.setProperty("https.proxyPort", proxyPort);
+			}
+
+			if (protocols.contains("ftp")) {
+				LOGGER.info(String.format("HTTP client will use ftp proxies: %s:%s", proxyHost, proxyPort));
+
+				System.setProperty("ftp.proxyHost", proxyHost);
+				System.setProperty("ftp.proxyPort", proxyPort);
+			}
+
+			if (protocols.contains("socks")) {
+				LOGGER.info(String.format("HTTP client will use socks proxies: %s:%s", proxyHost, proxyPort));
+				System.setProperty("socksProxyHost", proxyHost);
+				System.setProperty("socksProxyPort", proxyPort);
+			}
 		}
 	}
 }
