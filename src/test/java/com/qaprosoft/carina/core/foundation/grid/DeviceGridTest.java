2ed6cc13c3f610d7b19b64fc43e3167bf69332eb
vdelendik <vadim.delendik@gmail.com>
2016-09-02 15:39:25 +0300

    improved test logic for DeviceGrid

--- src/test/java/com/qaprosoft/carina/core/foundation/grid/DeviceGridTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/grid/DeviceGridTest.java
@@ -10,8 +10,16 @@ public class DeviceGridTest
 	@Test
 	public void testDeviceGrid()
 	{
+		DevicePool.registerDevices();
 		final long threadId = Thread.currentThread().getId();
 		DevicePool.registerDevice2Thread(threadId);
+		
+		try {
+			Thread.sleep(15*1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		DevicePool.deregisterDeviceByThread(threadId);
 	}
 }
