79d132293120739245a54085d7528e126c9b4cce
igorv22 <igorv22@yahoo.com>
2016-09-03 15:18:01 +0300

    - removed test

--- src/test/java/com/qaprosoft/carina/core/foundation/grid/DeviceGridTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/grid/DeviceGridTest.java
@@ -1,25 +1,22 @@
 package com.qaprosoft.carina.core.foundation.grid;
 
-import org.testng.annotations.Test;
-
-import com.qaprosoft.carina.core.foundation.webdriver.device.DevicePool;
 
 
 public class DeviceGridTest
 {
-	@Test
-	public void testDeviceGrid()
-	{
-		DevicePool.registerDevices();
-		final long threadId = Thread.currentThread().getId();
-		DevicePool.registerDevice2Thread(threadId);
-		
-		try {
-			Thread.sleep(15*1000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		DevicePool.deregisterDeviceByThread(threadId);
-	}
+//	@Test
+//	public void testDeviceGrid()
+//	{
+//		DevicePool.registerDevices();
+//		final long threadId = Thread.currentThread().getId();
+//		DevicePool.registerDevice2Thread(threadId);
+//		
+//		try {
+//			Thread.sleep(15*1000);
+//		} catch (InterruptedException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		DevicePool.deregisterDeviceByThread(threadId);
+//	}
 }
