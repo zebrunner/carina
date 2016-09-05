937e2b4f07dee98c35dfb5016cc7e6db1b6519ab
igorv22 <igorv22@yahoo.com>
2016-09-05 16:04:19 +0300

    - grid fixes

--- src/test/java/com/qaprosoft/carina/core/foundation/grid/DeviceGridTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/grid/DeviceGridTest.java
@@ -1,22 +1,18 @@
 package com.qaprosoft.carina.core.foundation.grid;
 
+import org.testng.annotations.Test;
 
+import com.qaprosoft.carina.core.foundation.webdriver.device.DevicePool;
 
 public class DeviceGridTest
 {
-//	@Test
-//	public void testDeviceGrid()
-//	{
-//		DevicePool.registerDevices();
-//		final long threadId = Thread.currentThread().getId();
-//		DevicePool.registerDevice2Thread(threadId);
-//		
-//		try {
-//			Thread.sleep(15*1000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//		DevicePool.deregisterDeviceByThread(threadId);
-//	}
+	@Test(enabled=false)
+	public void testDeviceGrid() throws InterruptedException
+	{
+		DevicePool.registerDevices();
+		final long threadId = Thread.currentThread().getId();
+		DevicePool.registerDevice2Thread(threadId);
+		Thread.sleep(5 * 1000);
+		DevicePool.deregisterDeviceByThread(threadId);
+	}
 }
