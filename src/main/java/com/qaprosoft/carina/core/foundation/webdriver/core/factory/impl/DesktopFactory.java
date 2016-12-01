3dd1d7d2172d6ff7e252722b5267ec4e0ce77999
Sergei Zagriychuk <serge@onflydev.com>
2016-12-01 17:14:34 +0300

    Updates in source code to minimize differences count and make merge into master branch.

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/DesktopFactory.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/DesktopFactory.java
@@ -1,50 +1,26 @@
 package com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import org.apache.commons.lang3.StringUtils;
-import org.openqa.selenium.Proxy;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 import com.qaprosoft.carina.core.foundation.webdriver.core.capability.CapabilitiesLoder;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.ChromeCapabilities;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.FirefoxCapabilities;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.HTMLUnitCapabilities;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.IECapabilities;
-import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.SafariCapabilities;
+import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.desktop.*;
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DesktopFactory extends AbstractFactory {
-
-	private static DesiredCapabilities staticCapabilities;
 
     @Override
     public WebDriver create(String testName, Device device) {
         RemoteWebDriver driver;
         String selenium = Configuration.get(Parameter.SELENIUM_HOST);
-
         DesiredCapabilities capabilities = getCapabilities(testName);
-		// setting proxy server if needed
-		String PROXY = Configuration.get(Parameter.PROXY_SERVER);
-		if (!StringUtils.EMPTY.equals(PROXY))
-		{
-			Proxy proxy = new Proxy();
-			proxy.setHttpProxy(PROXY).setFtpProxy(PROXY).setSslProxy(PROXY);
-			capabilities.setCapability(CapabilityType.PROXY, proxy);
-		}
-		// setting static capabilities if not empty
-		if (staticCapabilities != null)
-		{
-			capabilities.merge(staticCapabilities);
-		}
 
         try {
             driver = new RemoteWebDriver(new URL(selenium), capabilities);
@@ -77,14 +53,6 @@ public class DesktopFactory extends AbstractFactory {
                 throw new RuntimeException("Unsupported browser: " + browser);
             }
         }
-    }
 
-	public static void addCapability(String name, Object value)
-	{
-		if (staticCapabilities == null)
-		{
-			staticCapabilities = new DesiredCapabilities();
-		}
-		staticCapabilities.setCapability(name, value);
-	}
+    }
 }
