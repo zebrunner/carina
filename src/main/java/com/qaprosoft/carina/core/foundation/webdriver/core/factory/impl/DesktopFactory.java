31011e0e3dbcad69dbcf8334c02276a0a8b78dae
Sergei Zagriychuk <sergeizagriychuk@gmail.com>
2016-12-08 17:38:35 +0300

    Desktop factory was formatted

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/DesktopFactory.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/DesktopFactory.java
@@ -19,53 +19,64 @@ import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.deskt
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
 
-public class DesktopFactory extends AbstractFactory {
+public class DesktopFactory extends AbstractFactory
+{
 
 	private static DesiredCapabilities staticCapabilities;
 
-    @Override
-    public WebDriver create(String testName, Device device) {
-        RemoteWebDriver driver;
-        String selenium = Configuration.get(Parameter.SELENIUM_HOST);
-        DesiredCapabilities capabilities = getCapabilities(testName);
+	@Override
+	public WebDriver create(String testName, Device device)
+	{
+		RemoteWebDriver driver;
+		String selenium = Configuration.get(Parameter.SELENIUM_HOST);
+		DesiredCapabilities capabilities = getCapabilities(testName);
 		if (staticCapabilities != null)
 		{
 			LOGGER.info("Static DesiredCapabilities will be merged to basic driver capabilities");
 			capabilities.merge(staticCapabilities);
 		}
 
-        try {
-            driver = new RemoteWebDriver(new URL(selenium), capabilities);
-        } catch (MalformedURLException e) {
-            throw new RuntimeException("Unable to create desktop driver");
-        }
-        return driver;
-    }
+		try
+		{
+			driver = new RemoteWebDriver(new URL(selenium), capabilities);
+		} catch (MalformedURLException e)
+		{
+			throw new RuntimeException("Unable to create desktop driver");
+		}
+		return driver;
+	}
 
+	public DesiredCapabilities getCapabilities(String testName)
+	{
+		String customCapabilities = Configuration.get(Parameter.CUSTOM_CAPABILITIES);
+		if (!customCapabilities.isEmpty())
+		{
+			return new CapabilitiesLoder().loadCapabilities(customCapabilities);
+		} else
+		{
+			String browser = Configuration.get(Parameter.BROWSER);
 
-    public DesiredCapabilities getCapabilities(String testName) {
-        String customCapabilities = Configuration.get(Parameter.CUSTOM_CAPABILITIES);
-        if (!customCapabilities.isEmpty()) {
-            return new CapabilitiesLoder().loadCapabilities(customCapabilities);
-        } else {
-            String browser = Configuration.get(Parameter.BROWSER);
-
-            if (BrowserType.FIREFOX.equalsIgnoreCase(browser)) {
-                return new FirefoxCapabilities().getCapability(testName);
-            } else if (BrowserType.IEXPLORE.equalsIgnoreCase(browser) || BrowserType.IE.equalsIgnoreCase(browser)
-                    || browser.equalsIgnoreCase("ie")) {
-                return new IECapabilities().getCapability(testName);
-            } else if (BrowserType.HTMLUNIT.equalsIgnoreCase(browser)) {
-                return new HTMLUnitCapabilities().getCapability(testName);
-            } else if (BrowserType.SAFARI.equalsIgnoreCase(browser)) {
-                return new SafariCapabilities().getCapability(testName);
-            } else if (BrowserType.CHROME.equalsIgnoreCase(browser)) {
-                return new ChromeCapabilities().getCapability(testName);
-            } else {
-                throw new RuntimeException("Unsupported browser: " + browser);
-            }
-        }
-    }
+			if (BrowserType.FIREFOX.equalsIgnoreCase(browser))
+			{
+				return new FirefoxCapabilities().getCapability(testName);
+			} else if (BrowserType.IEXPLORE.equalsIgnoreCase(browser) || BrowserType.IE.equalsIgnoreCase(browser) || browser.equalsIgnoreCase("ie"))
+			{
+				return new IECapabilities().getCapability(testName);
+			} else if (BrowserType.HTMLUNIT.equalsIgnoreCase(browser))
+			{
+				return new HTMLUnitCapabilities().getCapability(testName);
+			} else if (BrowserType.SAFARI.equalsIgnoreCase(browser))
+			{
+				return new SafariCapabilities().getCapability(testName);
+			} else if (BrowserType.CHROME.equalsIgnoreCase(browser))
+			{
+				return new ChromeCapabilities().getCapability(testName);
+			} else
+			{
+				throw new RuntimeException("Unsupported browser: " + browser);
+			}
+		}
+	}
 
 	public static void addStaticCapability(String name, Object value)
 	{
