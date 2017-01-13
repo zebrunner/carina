3545a3d75a7a9f27381ec8190a511007af06ac40
vdelendik <vadim.delendik@gmail.com>
2017-01-14 01:51:54 +0300

    formatting browserMobProxy test

--- src/test/java/com/qaprosoft/carina/core/foundation/browsermob/BrowserMobTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/browsermob/BrowserMobTest.java
@@ -12,28 +12,26 @@ import com.qaprosoft.carina.core.foundation.webdriver.DriverPool;
 
 import net.lightbody.bmp.BrowserMobProxy;
 
-public class BrowserMobTest
-{
+public class BrowserMobTest {
 	private static String header = "my_header";
 	private static String headerValue = "my_value";
-	
+
 	@BeforeClass(alwaysRun = true)
 	public void beforeClass() {
-		//do nothing
+		// do nothing
 		R.CONFIG.put("browsermob_proxy", "true");
-		
+
 		HttpClient.setupProxy();
-		
+
 		BrowserMobProxy proxy = DriverPool.getBrowserMobProxy();
 		proxy.addHeader(header, headerValue);
 	}
 
 	@Test
-	public void testBrowserModheader() //throws NoSuchAlgorithmException, NoSuchPaddingException, IOException, URISyntaxException
-	{
+	public void testBrowserModheader() {
 		Map<String, String> headers = DriverPool.getBrowserMobProxy().getAllHeaders();
 		Assert.assertTrue(headers.containsKey(header), "Tehre is no custom header: " + header);
 		Assert.assertTrue(headers.get(header).equals(headerValue), "Tehre is no custom header value: " + headerValue);
 	}
-	
+
 }
