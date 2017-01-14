3f749af31ba0a01b026247f3a095453fc750977c
vdelendik <vadim.delendik@gmail.com>
2017-01-14 04:51:23 +0300

    unit test coverage improvements

--- src/test/java/com/qaprosoft/carina/core/foundation/jira/JiraTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/jira/JiraTest.java
@@ -11,31 +11,49 @@ import org.testng.annotations.Test;
 import com.qaprosoft.carina.core.foundation.utils.R;
 
 public class JiraTest {
+	private static final String customTicket1 = "bug1";
+	private static final String customTicket2 = "bug2";
+	private static final String customTicket3 = "Ticket From Test Description";
 
 	@Test
 	public void testJiraTickets() {
 		Assert.assertNull(Jira.jiraTickets.get());
 
-		Jira.setTickets("bug1");
+		Jira.setTickets(customTicket1);
 		Assert.assertEquals(Jira.jiraTickets.get().size(), 1);
 
 		Jira.clearTickets();
 		Assert.assertNull(Jira.jiraTickets.get());
 
 		List<String> tickets = new ArrayList<String>();
-		tickets.add("bug1");
-		tickets.add("bug2");
+		tickets.add(customTicket1);
+		tickets.add(customTicket2);
 		Jira.setTickets(tickets);
 		Assert.assertEquals(Jira.jiraTickets.get().size(), 2);
 	}
 
+	
+	@Test(description="JIRA# " + customTicket3)
+	public void testJiraTicketFromTestDescriptionAnnotation() {
+		//do nothing. verification is in AfterMethod
+	}
+	
 	@AfterMethod
 	public void testGetJiraTickets(ITestResult result) {
-		R.CONFIG.put("track_known_issues", "true");
-		List<String> tickets = Jira.getTickets(result);
-		Assert.assertTrue(tickets.contains("bug1"));
-		Assert.assertTrue(tickets.contains("bug2"));
+		if (result.getMethod().getMethodName().equals("testJiraTickets")) {
+			R.CONFIG.put("track_known_issues", "true");
+			List<String> tickets = Jira.getTickets(result);
+			Assert.assertTrue(tickets.contains(customTicket1));
+			Assert.assertTrue(tickets.contains(customTicket2));
 
-		Assert.assertTrue(Jira.isRetryDisabled(result));
+			Assert.assertTrue(Jira.isRetryDisabled(result));
+		}
+		
+		if (result.getMethod().getMethodName().equals("testJiraTicketFromTestDescriptionAnnotation")) {
+			List<String> tickets = Jira.getTickets(result);
+			Assert.assertTrue(tickets.contains(customTicket3));
+
+		}
+		
 	}
 }
