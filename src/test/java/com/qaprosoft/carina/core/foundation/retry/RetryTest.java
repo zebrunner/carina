ac3287a9e9e633afbe7329388d8d73acc6462739
vdelendik <vadim.delendik@gmail.com>
2017-01-14 06:09:39 +0300

    fixes to retryAnalyzer unit tests

--- src/test/java/com/qaprosoft/carina/core/foundation/retry/RetryTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/retry/RetryTest.java
@@ -1,14 +1,12 @@
 package com.qaprosoft.carina.core.foundation.retry;
 
 import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import com.qaprosoft.carina.core.foundation.utils.R;
-import com.qaprosoft.carina.core.foundation.utils.naming.TestNamingUtil;
 
 public class RetryTest {
+	RetryAnalyzer retryAnalyzer = new RetryAnalyzer();
 
 	@Test
 	public void testRetryCounter() {
@@ -23,14 +21,28 @@ public class RetryTest {
 		Assert.assertEquals(count, 1);
 	}
 
-	@AfterMethod
-	public void testRetryAnalyzer(ITestResult result) {
+	@Test
+	public void testGetMaxRetryCountForTest() {
 		R.CONFIG.put("retry_count", "1");
-		RetryCounter.initCounter(TestNamingUtil.getCanonicalTestName(result));
-
-		RetryAnalyzer retryAnalyzer = new RetryAnalyzer();
-		Assert.assertEquals(retryAnalyzer.retry(result), true);
-		Assert.assertEquals(retryAnalyzer.retry(result), false);
+		Assert.assertEquals(RetryAnalyzer.getMaxRetryCountForTest(), 1);
 	}
 
+/*	@AfterMethod
+	public void testRetryAnalyzer(ITestResult result) {
+		if (result.getMethod().getMethodName().equals("testRetryCounter")) {
+			String test = TestNamingUtil.getCanonicalTestName(result);
+			R.CONFIG.put("retry_count", "1");
+			R.CONFIG.put("track_known_issues", "true");
+			
+			RetryCounter.initCounter(test);
+			int count = RetryCounter.getRunCount(test);
+			Assert.assertEquals(count, 0);
+
+			Assert.assertEquals(retryAnalyzer.retry(result), true);
+			count = RetryCounter.getRunCount(test);
+			Assert.assertEquals(count, 1);
+			Assert.assertEquals(retryAnalyzer.retry(result), false);
+		}
+	}
+*/
 }
