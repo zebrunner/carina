8656ec0ff7ce27d56f96072fab8e7d3b6e540d80
Yauheni Patotski <yauhenipatotski@yauhenis-mbp-2.minsk.epam.com>
2017-04-12 18:01:34 +0300

    init metadata

--- src/main/java/com/qaprosoft/carina/core/foundation/webdriver/ExtendedCommandExecutor.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/ExtendedCommandExecutor.java
@@ -1,33 +1,39 @@
 package com.qaprosoft.carina.core.foundation.webdriver;
 
-import java.io.IOException;
-
+import com.qaprosoft.carina.core.foundation.webdriver.metadata.MetaDataCollector;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.CommandExecutor;
+import org.openqa.selenium.remote.DriverCommand;
 import org.openqa.selenium.remote.Response;
 
-public class ExtendedCommandExecutor implements CommandExecutor{
-	protected static final Logger LOGGER = Logger.getLogger(ExtendedCommandExecutor.class);
-	
-	private CommandExecutor commandExecutor;
-	
-	protected CommandExecutor getCommandExecutor() {
+import java.io.IOException;
+
+public class ExtendedCommandExecutor implements CommandExecutor {
+    protected static final Logger LOGGER = Logger.getLogger(ExtendedCommandExecutor.class);
+
+    private CommandExecutor commandExecutor;
+
+    protected CommandExecutor getCommandExecutor() {
         return commandExecutor;
     }
 
     protected void setCommandExecutor(CommandExecutor commandExecutor) {
         this.commandExecutor = commandExecutor;
     }
-    
+
     public ExtendedCommandExecutor(CommandExecutor commandExecutor) {
         this.commandExecutor = commandExecutor;
     }
 
-	@Override
-	public Response execute(Command command) throws IOException {
-		Response response = commandExecutor.execute(command);
-		return response;
-	}
+    @Override
+    public Response execute(Command command) throws IOException {
+        if (command.getName().equals(DriverCommand.CLICK_ELEMENT)|| command.getName().equals(DriverCommand.EXECUTE_SCRIPT)) {
+            //Here we are interested only in the send keys event (viz., type action)
+            MetaDataCollector.collectDataFromElement(commandExecutor, command);
+        }
+        Response response = commandExecutor.execute(command);
+        return response;
+    }
 
 }
---
+++ src/main/java/com/qaprosoft/carina/core/foundation/webdriver/metadata/Rectangle.java
@@ -0,0 +1,43 @@
+package com.qaprosoft.carina.core.foundation.webdriver.metadata;
+
+/**
+ * Created by yauhenipatotski on 4/12/17.
+ */
+public class Rectangle {
+    public int x;
+    public int y;
+    public int width;
+    public int height;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+}
