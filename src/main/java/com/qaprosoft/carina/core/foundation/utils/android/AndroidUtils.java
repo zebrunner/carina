01c9ec76eada6d09b5d94a173aa00f6b83524a05
Maksim Sarychau <msarychau@onflydev.com>
2017-06-02 02:13:15 +0200

    Adding new pressKeyCode method to new Appium

--- src/main/java/com/qaprosoft/carina/core/foundation/utils/android/AndroidUtils.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/utils/android/AndroidUtils.java
@@ -9,6 +9,7 @@ import java.util.concurrent.TimeUnit;
 import com.qaprosoft.carina.core.foundation.utils.android.recorder.utils.AdbExecutor;
 import com.qaprosoft.carina.core.foundation.utils.android.recorder.utils.CmdLine;
 import com.qaprosoft.carina.core.foundation.webdriver.device.DevicePool;
+import io.appium.java_client.*;
 import org.apache.log4j.Logger;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
@@ -24,11 +25,6 @@ import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 import com.qaprosoft.carina.core.foundation.webdriver.DriverPool;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.SwipeElementDirection;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDeviceActionShortcuts;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -82,11 +78,18 @@ public class AndroidUtils {
     protected boolean pressKeyCode(int keyCode) {
         try {
             LOGGER.info("Press key code: " + keyCode);
-            ((AndroidDeviceActionShortcuts) DriverPool.getDriver())
-                    .pressKeyCode(keyCode);
+            ((PressesKeyCode) DriverPool.getDriver()).pressKeyCode(keyCode);
             return true;
         } catch (Exception e) {
-            LOGGER.error("Exception:" + e);
+            LOGGER.error("Exception during pressKeyCode:", e);
+            try {
+                LOGGER.info("Press key code by old method: " + keyCode);
+                ((AndroidDeviceActionShortcuts) DriverPool.getDriver())
+                        .pressKeyCode(keyCode);
+                return true;
+            } catch (Exception err) {
+                LOGGER.error("Exception during pressKeyCode with old method:", err);
+            }
         }
         return false;
     }
