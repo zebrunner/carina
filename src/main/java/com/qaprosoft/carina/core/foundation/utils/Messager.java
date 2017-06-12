e6a4eeb7e5718469b41ce9860c71917b7f3f5b6a
vdelendik <vadim.delendik@gmail.com>
2017-06-12 19:32:05 +0300

    Add ed isVisible method implementation

--- src/main/java/com/qaprosoft/carina/core/foundation/utils/Messager.java
+++ src/main/java/com/qaprosoft/carina/core/foundation/utils/Messager.java
@@ -113,6 +113,10 @@ public enum Messager {
 	ELEMENT_BECOME_CLICKABLE("PASS: element '%s' become clickable."),
 
 	ELEMENT_NOT_BECOME_CLICKABLE("FAIL: element '%s' is not become clickable before timeout!"),
+	
+	ELEMENT_BECOME_VISIBLE("PASS: element '%s' become visible."),
+
+	ELEMENT_NOT_BECOME_VISIBLE("FAIL: element '%s' is not become visible before timeout!"),
 
 	ELEMENT_CLICKED("PASS: element '%s' is clicked."),
 
