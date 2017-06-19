2714e6b389d89775b6f54e4166c7405b59be1051
vdelendik <vadim.delendik@gmail.com>
2017-06-19 17:42:22 +0300

    fix for ZafiraClient constructor

--- src/test/java/com/qaprosoft/carina/core/foundation/zafira/ZafiraClientTest.java
+++ src/test/java/com/qaprosoft/carina/core/foundation/zafira/ZafiraClientTest.java
@@ -26,7 +26,9 @@ public class ZafiraClientTest
 	private TestSuiteType testSuite;
 	TestRunType testRun;
 	
-	private static final ZafiraClient zc = new ZafiraClient("http://stg.caronfly.com:8080/zafira", "admin", "admin");
+	//TODO: to enable tests we should incorporate zc.login somehow later
+	private static final ZafiraClient zc = new ZafiraClient("http://stg.caronfly.com:8080/zafira");
+	
 	
 	@Test(enabled=ENABLED)
 	public void testStatus()
