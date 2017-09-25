b684b0dffe9938214a120644ae7e17333fc02ccd
vdelendik <vadim.delendik@gmail.com>
2017-09-25 14:10:29 +0300

    1st step to support new capabilities for STF-Selenium Hub

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
@@ -1,6 +1,14 @@
 package com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl;
 
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+
 import com.qaprosoft.carina.core.foundation.commons.SpecialKeywords;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
@@ -11,17 +19,12 @@ import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobil
 import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobile.MobileWebCapabilities;
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
+import com.qaprosoft.carina.core.foundation.webdriver.device.DevicePool;
+
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.IOSElement;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
-
-import java.net.MalformedURLException;
-import java.net.URL;
 
 public class MobileFactory extends AbstractFactory {
 
@@ -42,25 +45,34 @@ public class MobileFactory extends AbstractFactory {
         RemoteWebDriver driver = null;
         DesiredCapabilities capabilities = getCapabilities(name, device);
         try {
-            if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE_GRID)) {
-                driver = new RemoteWebDriver(new URL(selenium), capabilities);
-            } else if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE) || driverType.equalsIgnoreCase(SpecialKeywords.MOBILE_POOL)) {
-                if (mobile_platform_name.toLowerCase().equalsIgnoreCase(SpecialKeywords.ANDROID)) {
-                    // use uiautomator2 for Android 7
-                    if (Configuration.getBoolean(Configuration.Parameter.ENABLE_AUTOMATOR2) && 
-                    		Configuration.get(Configuration.Parameter.MOBILE_PLATFORM_VERSION).trim().startsWith("7.")) {
-                        LOGGER.debug("uiautomator2 will be enabled for Android 7");
-                        capabilities.setCapability("automationName", "uiautomator2");
-                    }
-                    // handler in case app was installed via adb and there is no need to sign app using appium
-                    if (Configuration.getBoolean(Configuration.Parameter.MOBILE_APP_INSTALL) && !Configuration.getBoolean(Configuration.Parameter.MOBILE_APP_UNINSTALL)) {
-                    	capabilities.setCapability("app", "");
-                    }
-                    driver = new AndroidDriver<AndroidElement>(new URL(selenium), capabilities);
-                } else if (mobile_platform_name.toLowerCase().equalsIgnoreCase(SpecialKeywords.IOS)) {
-                    driver = new IOSDriver<IOSElement>(new URL(selenium), capabilities);
-                }
-            } else if (driverType.equalsIgnoreCase(SpecialKeywords.CUSTOM)) {
+			if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE)
+					|| driverType.equalsIgnoreCase(SpecialKeywords.MOBILE_POOL)
+					|| driverType.equalsIgnoreCase(SpecialKeywords.MOBILE_GRID)) {
+				if (mobile_platform_name.toLowerCase().equalsIgnoreCase(SpecialKeywords.ANDROID)) {
+					// use uiautomator2 for Android 7
+					if (Configuration.getBoolean(Configuration.Parameter.ENABLE_AUTOMATOR2) && Configuration
+							.get(Configuration.Parameter.MOBILE_PLATFORM_VERSION).trim().startsWith("7.")) {
+						LOGGER.debug("uiautomator2 will be enabled for Android 7");
+						capabilities.setCapability("automationName", "uiautomator2");
+					}
+					// handler in case app was installed via adb and there is no
+					// need to sign app using appium
+					if (Configuration.getBoolean(Configuration.Parameter.MOBILE_APP_INSTALL)
+							&& !Configuration.getBoolean(Configuration.Parameter.MOBILE_APP_UNINSTALL)) {
+						capabilities.setCapability("app", "");
+					}
+					driver = new AndroidDriver<AndroidElement>(new URL(selenium), capabilities);
+					
+					//TODO: refactor code to remote mobile_devices pool reference at all.
+					//String sid = ((RemoteWebDriver) driver).getSessionId().toString();
+					String udid = driver.getCapabilities().getCapability("udid").toString();
+					device = DevicePool.findDevice(udid);
+					device.connectRemote();
+					
+				} else if (mobile_platform_name.toLowerCase().equalsIgnoreCase(SpecialKeywords.IOS)) {
+					driver = new IOSDriver<IOSElement>(new URL(selenium), capabilities);
+				}
+			} else if (driverType.equalsIgnoreCase(SpecialKeywords.CUSTOM)) {
                 driver = new RemoteWebDriver(new URL(selenium), capabilities);
             } else {
                 throw new RuntimeException("Unsupported browser");
@@ -79,26 +91,29 @@ public class MobileFactory extends AbstractFactory {
 
     public DesiredCapabilities getCapabilities(String name, Device device) {
         String customCapabilities = Configuration.get(Parameter.CUSTOM_CAPABILITIES);
+        DesiredCapabilities capabilities = new DesiredCapabilities(); 
         if (!customCapabilities.isEmpty()) {
-            return new CapabilitiesLoder().loadCapabilities(customCapabilities);
+        	capabilities = new CapabilitiesLoder().loadCapabilities(customCapabilities);
         } else {
             String driverType = Configuration.get(Configuration.Parameter.DRIVER_TYPE);
 
             if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE_GRID)) {
-                return new MobileGridCapabilities().getCapability(name);
+            	capabilities = new MobileGridCapabilities().getCapability(name);
             } else if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE)
                     && !Configuration.get(Configuration.Parameter.BROWSER).isEmpty()) {
-                return new MobileWebCapabilities().getCapability(name);
+            	capabilities = new MobileWebCapabilities().getCapability(name);
             } else if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE)
                     && Configuration.get(Configuration.Parameter.BROWSER).isEmpty()) {
-                return new MobileNativeCapabilities().getCapability(name);
+            	capabilities = new MobileNativeCapabilities().getCapability(name);
             } else if (driverType.equalsIgnoreCase(SpecialKeywords.MOBILE_POOL)) {
             	//TODO: ensure that mobile_pool works for web testing too!
-            	return new MobilePoolCapabilities().getCapability(name, device);            	
+            	capabilities = new MobilePoolCapabilities().getCapability(name, device);            	
             }else {
                 throw new RuntimeException("Unsupported driver type:" + driverType);
             }
         }
+        
+		return capabilities;
 
     }
 
