6366e73f9c99ddd3aa0d387a3515343b56d2de98
vdelendik <vadim.delendik@gmail.com>
2017-10-07 17:03:54 +0300

    added logging for capabilities mobile platform names

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
@@ -60,6 +60,8 @@ public class MobileFactory extends AbstractFactory {
 							&& !Configuration.getBoolean(Configuration.Parameter.MOBILE_APP_UNINSTALL)) {
 						capabilities.setCapability("app", "");
 					}
+					LOGGER.info("platformName: " +  capabilities.getCapability("platformName"));
+					LOGGER.info("platform: " +  capabilities.getCapability("platform"));
 					driver = new AndroidDriver<AndroidElement>(new URL(seleniumHost), capabilities);
 					
 					if (device.isNull()) 
