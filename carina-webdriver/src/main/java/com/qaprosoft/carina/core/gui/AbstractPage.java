ffeb0255438e07c1c995fdc625e35e14058f946b
Tamikana <laragive@mail.ru>
2017-11-13 22:45:58 +0300

    cleanup webdriver gui

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/AbstractPage.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/AbstractPage.java
@@ -38,85 +38,100 @@ import com.qaprosoft.carina.core.foundation.webdriver.Screenshot;
  * 
  * @author Alex Khursevich
  */
-public abstract class AbstractPage extends AbstractUIObject {
-    protected String pageURL = Configuration.get(Parameter.URL);
+public abstract class AbstractPage extends AbstractUIObject
+{
+	protected String pageURL = Configuration.get(Parameter.URL);
 
-    public AbstractPage(WebDriver driver) {
-        super(driver);
-    }
+	public AbstractPage(WebDriver driver)
+	{
+		super(driver);
+	}
 
-    /**
-     * Opens page according to specified in constructor URL.
-     */
-    public void open() {
-        openURL(pageURL);
-    }
+	/**
+	 * Opens page according to specified in constructor URL.
+	 */
+	public void open()
+	{
+		openURL(pageURL);
+	}
 
-    protected void setPageURL(String relURL) {
-        String baseURL;
-        // if(!"NULL".equalsIgnoreCase(Configuration.get(Parameter.ENV)))
-        if (!Configuration.get(Parameter.ENV).isEmpty()) {
-            baseURL = Configuration.getEnvArg("base");
-        } else {
-            baseURL = Configuration.get(Parameter.URL);
-        }
-        pageURL = baseURL + relURL;
-    }
+	protected void setPageURL(String relURL)
+	{
+		String baseURL;
+		// if(!"NULL".equalsIgnoreCase(Configuration.get(Parameter.ENV)))
+		if (!Configuration.get(Parameter.ENV).isEmpty())
+		{
+			baseURL = Configuration.getEnvArg("base");
+		} else
+		{
+			baseURL = Configuration.get(Parameter.URL);
+		}
+		pageURL = baseURL + relURL;
+	}
 
-    protected void setPageAbsoluteURL(String url) {
-        this.pageURL = url;
-    }
+	protected void setPageAbsoluteURL(String url)
+	{
+		this.pageURL = url;
+	}
 
-    public String getPageURL() {
-        return pageURL;
-    }
+	public String getPageURL()
+	{
+		return pageURL;
+	}
 
-    public boolean isPageOpened() {
-        return isPageOpened(this);
-    }
+	public boolean isPageOpened()
+	{
+		return isPageOpened(this);
+	}
 
-    public boolean isPageOpened(long timeout) {
-        return isPageOpened(this, timeout);
-    }
+	public boolean isPageOpened(long timeout)
+	{
+		return isPageOpened(this, timeout);
+	}
 
-    public String savePageAsPdf(boolean scaled) throws IOException, DocumentException {
-        String pdfName = "";
+	public String savePageAsPdf(boolean scaled) throws IOException, DocumentException
+	{
+		String pdfName = "";
 
-        // Define test screenshot root
-        String test = "";
-        if (TestNamingUtil.isTestNameRegistered()) {
-            test = TestNamingUtil.getTestNameByThread();
-        } else {
-            test = "undefined";
-        }
+		// Define test screenshot root
+		String test = "";
+		if (TestNamingUtil.isTestNameRegistered()) {
+			test = TestNamingUtil.getTestNameByThread();
+		} else {
+			test = "undefined";
+		}
+		
+		File testRootDir = ReportContext.getTestDir();
+		File artifactsFolder = ReportContext.getArtifactsFolder();
 
-        File testRootDir = ReportContext.getTestDir();
-        File artifactsFolder = ReportContext.getArtifactsFolder();
+		String fileID = test.replaceAll("\\W+", "_") + "-" + System.currentTimeMillis();
+		pdfName = fileID + ".pdf";
 
-        String fileID = test.replaceAll("\\W+", "_") + "-" + System.currentTimeMillis();
-        pdfName = fileID + ".pdf";
+		String fullPdfPath = artifactsFolder.getAbsolutePath() + "/" + pdfName;
+		// TODO: test this implementation and change back to capture if necessary
+		Image image = Image.getInstance(testRootDir.getAbsolutePath() + "/" + Screenshot.captureFailure(driver, ""));
+		Document document = null;
+		if (scaled)
+		{
+			document = new Document(PageSize.A4, 10, 10, 10, 10);
+			if (image.getHeight() > (document.getPageSize().getHeight() - 20)
+					|| image.getScaledWidth() > (document.getPageSize().getWidth() - 20))
+			{
+				image.scaleToFit(document.getPageSize().getWidth() - 20, document.getPageSize().getHeight() - 20);
+			}
+		} else
+		{
+			document = new Document(new RectangleReadOnly(image.getScaledWidth(), image.getScaledHeight()));
+		}
+		PdfWriter.getInstance(document, new FileOutputStream(fullPdfPath));
+		document.open();
+		document.add(image);
+		document.close();
+		return fullPdfPath;
+	}
 
-        String fullPdfPath = artifactsFolder.getAbsolutePath() + "/" + pdfName;
-        // TODO: test this implementation and change back to capture if
-        // necessary
-        Image image = Image.getInstance(testRootDir.getAbsolutePath() + "/" + Screenshot.captureFailure(driver, ""));
-        Document document = null;
-        if (scaled) {
-            document = new Document(PageSize.A4, 10, 10, 10, 10);
-            if (image.getHeight() > (document.getPageSize().getHeight() - 20) || image.getScaledWidth() > (document.getPageSize().getWidth() - 20)) {
-                image.scaleToFit(document.getPageSize().getWidth() - 20, document.getPageSize().getHeight() - 20);
-            }
-        } else {
-            document = new Document(new RectangleReadOnly(image.getScaledWidth(), image.getScaledHeight()));
-        }
-        PdfWriter.getInstance(document, new FileOutputStream(fullPdfPath));
-        document.open();
-        document.add(image);
-        document.close();
-        return fullPdfPath;
-    }
-
-    public String savePageAsPdf() throws IOException, DocumentException {
-        return savePageAsPdf(true);
-    }
+	public String savePageAsPdf() throws IOException, DocumentException
+	{
+		return savePageAsPdf(true);
+	}
 }
