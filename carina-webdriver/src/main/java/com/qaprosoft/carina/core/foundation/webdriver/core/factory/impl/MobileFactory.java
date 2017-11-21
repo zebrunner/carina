a3fad4df084f9281e45c5ebb381d1f0a4945c55f
akhursevich <hursrevich@gmail.com>
2017-11-21 20:04:38 +0300

    - MobileFactory updated accordingly

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 
+import com.qaprosoft.carina.commons.models.RemoteDevice;
 import com.qaprosoft.carina.core.foundation.commons.SpecialKeywords;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
@@ -24,7 +25,6 @@ import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobil
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
 import com.qaprosoft.carina.core.foundation.webdriver.device.DevicePool;
-import com.qaprosoft.zafira.models.stf.STFDevice;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
@@ -73,8 +73,8 @@ public class MobileFactory extends AbstractFactory {
 					boolean stfEnabled = R.CONFIG.getBoolean(SpecialKeywords.CAPABILITIES + "." + SpecialKeywords.STF_ENABLED);
 							
 					if (stfEnabled) {
-						STFDevice info = getDeviceInfo(seleniumHost, driver.getSessionId().toString());
-						device.setRemoteURL((String)info.getRemoteConnectUrl());
+						RemoteDevice info = getDeviceInfo(seleniumHost, driver.getSessionId().toString());
+						device.setRemoteURL(info.getRemoteURL());
 						device.connectRemote();
 					} else {
 						//STF can be used in manual mode. In this case redefine deviceType and udid using values from config.properties
@@ -125,9 +125,9 @@ public class MobileFactory extends AbstractFactory {
 		return capabilities;
     }
     
-    private STFDevice getDeviceInfo(String seleniumHost, String sessionId)
+    private RemoteDevice getDeviceInfo(String seleniumHost, String sessionId)
 	{
-		STFDevice device = null;
+    		RemoteDevice device = null;
 		try
 		{
 			HttpClient client = HttpClientBuilder.create().build();
@@ -136,7 +136,7 @@ public class MobileFactory extends AbstractFactory {
 	        
 	        ObjectMapper mapper = new ObjectMapper();
 	        mapper.configure(DeserializationConfig.Feature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-	        device =  mapper.readValue(response.getEntity().getContent(), STFDevice.class);
+	        device =  mapper.readValue(response.getEntity().getContent(), RemoteDevice.class);
 		}
 		catch (Exception e) 
 		{
