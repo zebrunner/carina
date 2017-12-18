18e9b9f243551609d8c83d7893f309846732b7f4
vdelendik <vadim.delendik@gmail.com>
2017-12-18 03:30:35 +0300

    restored findElement logic for deprecated DriverHelper->isElementPresent

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/DriverHelper.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/DriverHelper.java
@@ -36,7 +36,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Wait;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -355,14 +354,13 @@ public class DriverHelper {
 		final WebDriver drv = getDriver();
 		wait = new WebDriverWait(drv, timeout, RETRY_TIME);
 		try {
-			//setImplicitTimeout(Math.max(1, Configuration.getLong(Parameter.RETRY_INTERVAL)/1000));
-			//wait.until((Function<WebDriver, Object>) dr -> !dr.findElements(by).isEmpty() && dr.findElement(by).isDisplayed());
-			wait.until(ExpectedConditions.presenceOfElementLocated(by));
+			setImplicitTimeout(0);
+			wait.until((Function<WebDriver, Object>) dr -> !dr.findElements(by).isEmpty() && dr.findElement(by).isDisplayed());
 			result = true;
 		} catch (Exception e) {
 			result = false;
 		} finally {
-			//setImplicitTimeout(IMPLICIT_TIMEOUT);	
+			setImplicitTimeout(IMPLICIT_TIMEOUT);	
 		}
 		
 		return result;
