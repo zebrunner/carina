ffdd9a597dad30fa6e37c7f0d9c9fa0d0acecb96
Aliaksei Khursevich <hursrevich@gmail.com>
2017-12-22 12:34:45 +0300

    - Qaprosoft copyrights updated

--- carina-reporting/src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/core/APIClient.java
+++ carina-reporting/src/main/java/com/qaprosoft/carina/core/foundation/report/testrail/core/APIClient.java
@@ -1,13 +1,18 @@
-/**
- * TestRail API binding for Java (API v2, available since TestRail 3.0)
+/*******************************************************************************
+ * Copyright 2013-2018 QaProSoft (http://www.qaprosoft.com).
  *
- * Learn more:
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * http://docs.gurock.com/testrail-api2/start
- * http://docs.gurock.com/testrail-api2/accessing
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- * Copyright Gurock Software GmbH. See license.md for details.
- */
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
  
 package com.qaprosoft.carina.core.foundation.report.testrail.core;
 
