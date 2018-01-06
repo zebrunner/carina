6002a1861e4b91aed9357bdc25b620bc40368451
vdelendik <vadim.delendik@gmail.com>
2018-01-06 03:55:40 +0300

    DriverHelper.getDriver() - added improved verification onto session is null driver

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/DriverHelper.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/DriverHelper.java
@@ -1403,16 +1403,22 @@ public class DriverHelper {
 	}
 
 	protected WebDriver getDriver() {
-		if (driver != null && !driver.toString().contains("null")) {
-			return driver;
+		try {
+			if (driver != null && !driver.toString().contains("null")) {
+				return driver;
+			}
+		} catch (Exception e) {
+			if (!e.getMessage().contains("Session ID is null.")) {
+				throw e;
+			}
+			// otherwise do nothing
 		}
+		
 		LOGGER.info("Unable to find driver in DriverHelper without DriverPool!");
 		
-		long currentThreadId = Thread.currentThread().getId();
-		if (driver == null || driver.toString().contains("null")) {
-			driver = DriverPool.getDriver();
-		}
+		driver = DriverPool.getDriver();
 		if (driver == null) {
+			long currentThreadId = Thread.currentThread().getId();
 			LOGGER.error("There is no any initialized driver for thread: " + currentThreadId);
 			throw new RuntimeException("Driver isn't initialized.");
 		}
