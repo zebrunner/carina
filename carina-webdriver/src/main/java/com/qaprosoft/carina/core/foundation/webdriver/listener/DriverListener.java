bcb8a90de658d0e89d6d30eb30cf140114391bec
vdelendik <vadim.delendik@gmail.com>
2018-05-29 17:06:54 +0300

    removed DriverListener exception for "An element could not be located on the page using the given search parameters" use-case

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/DriverListener.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/DriverListener.java
@@ -153,7 +153,8 @@ public class DriverListener implements IConfigurableEventListener {
 			if (!thr.getMessage().contains("StaleObjectException")
 					&& !thr.getMessage().contains("InvalidElementStateException")
 					&& !thr.getMessage().contains("stale element reference")
-					&& !thr.getMessage().contains("no such element: Unable to locate element") 
+					&& !thr.getMessage().contains("no such element: Unable to locate element")
+					&& !thr.getMessage().contains("An element could not be located on the page using the given search parameters")
 					//&& !thr.getMessage().contains("timeout")
 					&& !thr.getMessage().contains("Session timed out or not found")) {
 				captureScreenshot(thr.getMessage(), driver, null, true);
