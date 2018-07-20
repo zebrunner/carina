8620d9952ce7a10b4cb3912c0ac394c63d08c306
vdelendik <vadim.delendik@gmail.com>
2018-07-20 16:10:17 +0300

    return slotCapabilities for iOS as well in selenium-hub

--- carina-grid/src/main/java/com/qaprosoft/carina/grid/MobileRemoteProxy.java
+++ carina-grid/src/main/java/com/qaprosoft/carina/grid/MobileRemoteProxy.java
@@ -19,6 +19,7 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
 
+import org.apache.commons.lang3.StringUtils;
 import org.openqa.grid.common.RegistrationRequest;
 import org.openqa.grid.internal.GridRegistry;
 import org.openqa.grid.internal.TestSession;
@@ -86,7 +87,12 @@ public class MobileRemoteProxy extends DefaultRemoteProxy {
         String udid = String.valueOf(session.getSlot().getCapabilities().get("udid"));
         if (STF.isSTFRequired(session.getSlot().getCapabilities(), session.getRequestedCapabilities())) {
             STF.reserveDevice(udid);
-            session.getRequestedCapabilities().put("slotCapabilities", getSlotCapabilities(session, udid));
+            //session.getRequestedCapabilities().put("slotCapabilities", getSlotCapabilities(session, udid));
+        }
+        
+        if (!StringUtils.isEmpty(udid)) {
+        	// this is our mobile Android or iOS device
+        	session.getRequestedCapabilities().put("slotCapabilities", getSlotCapabilities(session, udid));
         }
     }
 
@@ -105,15 +111,16 @@ public class MobileRemoteProxy extends DefaultRemoteProxy {
 		// get existing slot capabilities from session
 		slotCapabilities.putAll(session.getSlot().getCapabilities());
 
-		// get remoteURL from STF device and add into custom slotCapabilities map
-		String remoteURL = null;
-
-		STFDevice stfDevice = STF.getDevice(udid);
-		if (stfDevice != null) {
-			LOGGER.fine("Identified '" + stfDevice.getModel() + "' device by udid: " + udid);
-			remoteURL = (String) stfDevice.getRemoteConnectUrl();
-			LOGGER.fine("Identified remoteURL '" + remoteURL + "' by udid: " + udid);
-			slotCapabilities.put("remoteURL", remoteURL);
+		if (STF.isSTFRequired(session.getSlot().getCapabilities(), session.getRequestedCapabilities())) {
+			// get remoteURL from STF device and add into custom slotCapabilities map
+			String remoteURL = null;
+			STFDevice stfDevice = STF.getDevice(udid);
+			if (stfDevice != null) {
+				LOGGER.fine("Identified '" + stfDevice.getModel() + "' device by udid: " + udid);
+				remoteURL = (String) stfDevice.getRemoteConnectUrl();
+				LOGGER.fine("Identified remoteURL '" + remoteURL + "' by udid: " + udid);
+				slotCapabilities.put("remoteURL", remoteURL);
+			}
 		}
 
 		return slotCapabilities;
