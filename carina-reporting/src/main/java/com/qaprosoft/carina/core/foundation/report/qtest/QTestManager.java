1498d3a23c15eb60c49fd6b1dbf057057b22e89f
vdelendik <vadim.delendik@gmail.com>
2018-11-10 12:23:51 +0300

    added initial qTest related tags declaration

---
+++ carina-reporting/src/main/java/com/qaprosoft/carina/core/foundation/report/qtest/QTestManager.java
@@ -0,0 +1,71 @@
+/*******************************************************************************
+ * Copyright 2013-2018 QaProSoft (http://www.qaprosoft.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+package com.qaprosoft.carina.core.foundation.report.qtest;
+
+import java.lang.reflect.Method;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.testng.ITestResult;
+
+public class QTestManager {
+    protected static final Logger LOGGER = Logger.getLogger(QTestManager.class);
+
+    private QTestManager() {
+    }
+
+    public static Set<String> getTestCases(ITestResult result) {
+
+        // Get a handle to the class and method
+        Class<?> testClass;
+        Set<String> testCases = new HashSet<String>();
+        try {
+            testClass = Class.forName(result.getMethod().getTestClass().getName());
+
+            // We can't use getMethod() because we may have parameterized tests
+            // for which we won't know the matching signature
+            String methodName = result.getMethod().getMethodName();
+            Method testMethod = null;
+            Method[] possibleMethods = testClass.getMethods();
+            for (Method possibleMethod : possibleMethods) {
+                if (possibleMethod.getName().equals(methodName)) {
+                    testMethod = possibleMethod;
+                    break;
+                }
+            }
+
+            if (testMethod != null) {
+                if (testMethod.isAnnotationPresent(QTestTestCase.class)) {
+                	QTestTestCase methodAnnotation = testMethod.getAnnotation(QTestTestCase.class);
+                	testCases.add(methodAnnotation.id());
+                	LOGGER.debug("qTest test case id '" + methodAnnotation.id() + "' is registered.");
+                }
+                if (testMethod.isAnnotationPresent(QTestTestCase.List.class)) {
+                	QTestTestCase.List methodAnnotation = testMethod.getAnnotation(QTestTestCase.List.class);
+                    for (QTestTestCase tcLocal : methodAnnotation.value()) {
+                    	testCases.add(tcLocal.id());
+                    	LOGGER.debug("qTest test case id '" + tcLocal.id() + "' is registered.");
+                   }
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            LOGGER.error(e);
+        }
+        return testCases;
+    }
+
+}
