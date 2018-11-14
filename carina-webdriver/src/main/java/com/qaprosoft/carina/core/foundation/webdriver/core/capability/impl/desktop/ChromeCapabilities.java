c4addc8e1544dd668c63eddf8e60b23388f4920b
vdelendik <vadim.delendik@gmail.com>
2018-11-14 21:36:30 +0300

    disabled chrome proxy setup via capabilities options

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/desktop/ChromeCapabilities.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/desktop/ChromeCapabilities.java
@@ -58,12 +58,12 @@ public class ChromeCapabilities extends AbstractCapabilities {
         // [VD] no need to set proxy via options anymore!
         // moreover if below code is uncommented then we have double proxy start and mess in host:port values
 
-        Proxy proxy = setupProxy();
+/*        Proxy proxy = setupProxy();
         if (proxy != null) {
         	// explicitely add proxy as chrome option
         	// https://github.com/SeleniumHQ/selenium/issues/5299
         	options.setProxy(proxy);
-        }
+        }*/
 
         
         capabilities.setCapability(ChromeOptions.CAPABILITY, options);
