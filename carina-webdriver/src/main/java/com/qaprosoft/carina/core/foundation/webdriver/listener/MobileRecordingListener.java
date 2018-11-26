6641e6d0177f4d722c920b0e5205ae337c2109ab
Nikolai Krasnik <nkrasnik@qaprosoft.com>
2018-11-26 16:53:12 +0300

    Removed async uploading

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/MobileRecordingListener.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/MobileRecordingListener.java
@@ -77,7 +77,8 @@ public class MobileRecordingListener<O1 extends BaseStartScreenRecordingOptions,
 											(BaseStopScreenRecordingOptions) stopRecordingOpt).getValue()))
 							.getValue().toString();
 					LOGGER.debug("Video will be uploaded to ftp. Test thread ID : " + Thread.currentThread().getId());
-					CompletableFuture.runAsync(() -> {uploadToFTP(data);});
+//					CompletableFuture.runAsync(() -> {uploadToFTP(data);});
+					uploadToFTP(data);
 					if (ZafiraSingleton.INSTANCE.isRunning()) {
 						ZafiraSingleton.INSTANCE.getClient().addTestArtifact(videoArtifact);
 					}
