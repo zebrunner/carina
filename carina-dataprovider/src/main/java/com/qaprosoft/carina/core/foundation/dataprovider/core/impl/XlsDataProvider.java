0fc907a46d0c1b70b56be1e44f913ebaca6537d0
Bogdan Rutskov <rutkovba@gmail.com>
2019-03-21 11:21:47 +0300

    Added google spreadsheet data supporting

--- carina-dataprovider/src/main/java/com/qaprosoft/carina/core/foundation/dataprovider/core/impl/XlsDataProvider.java
+++ carina-dataprovider/src/main/java/com/qaprosoft/carina/core/foundation/dataprovider/core/impl/XlsDataProvider.java
@@ -19,6 +19,7 @@ import java.lang.annotation.Annotation;
 import java.util.Arrays;
 import java.util.Map;
 
+import com.qaprosoft.carina.core.foundation.dataprovider.parser.SpreadsheetParser;
 import org.testng.ITestContext;
 import org.testng.ITestNGMethod;
 
@@ -45,8 +46,9 @@ public class XlsDataProvider extends BaseDataProvider {
         DSBean dsBean = new DSBean(parameters, context
                 .getCurrentXmlTest().getAllParameters());
 
-        XLSTable dsData = XLSParser.parseSpreadSheet(dsBean.getDsFile(),
-                dsBean.getXlsSheet(), dsBean.getExecuteColumn(), dsBean.getExecuteValue());
+        XLSTable dsData = dsBean.isSpreadsheet() ?
+                SpreadsheetParser.parseSpreadSheet(dsBean.getDsFile(), dsBean.getXlsSheet(), dsBean.getExecuteColumn(), dsBean.getExecuteValue()) :
+                XLSParser.parseSpreadSheet(dsBean.getDsFile(), dsBean.getXlsSheet(), dsBean.getExecuteColumn(), dsBean.getExecuteValue());
 
         argsList = dsBean.getArgs();
         staticArgsList = dsBean.getStaticArgs();
