76bfa1726420e03e40402666366c97192351b238
vdelendik <vadim.delendik@gmail.com>
2019-11-28 00:39:24 +0300

    excluded "Session does not exist" from making screenshot

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/Screenshot.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/Screenshot.java
@@ -623,6 +623,7 @@ public class Screenshot {
 				|| message.contains("Session ID is null. Using WebDriver after calling quit")
 				|| message.contains("A session is either terminated or not started")
                 || message.contains("invalid session id")
+                || message.contains("Session does not exist")
 				|| message.contains("Session timed out or not found")
 				|| message.contains("cannot forward the request")
                 || message.contains("connect ECONNREFUSED")
