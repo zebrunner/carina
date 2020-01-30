c606c03894e5fdfc5da6e0168ff241682d5796c7
Sergei Zagriychuk <sergeizagriychuk@gmail.com>
2020-01-30 18:43:39 +0300

    additional okhttp client logging updates

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/httpclient/HttpClientLoggingInterceptor.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/httpclient/HttpClientLoggingInterceptor.java
@@ -34,12 +34,12 @@ public class HttpClientLoggingInterceptor implements Interceptor {
 
 		long t1 = System.nanoTime();
 		LOGGER.info(
-				String.format("Sending request %s %s on %s%n%s%n%s", request.method(), request.url(), chain.connection(), request.headers(), bodyToString(request)));
+				String.format("Sending REQUEST %s %s on %s%n%s%n%s", request.method(), request.url(), chain.connection(), request.headers(), bodyToString(request)));
 
 		Response response = chain.proceed(request);
 
 		long t2 = System.nanoTime();
-		LOGGER.info(String.format("Received response for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d,
+		LOGGER.info(String.format("Received RESPONSE for %s in %.1fms%n%s", response.request().url(), (t2 - t1) / 1e6d,
 				response.headers()));
 
 		return response;
@@ -50,10 +50,18 @@ public class HttpClientLoggingInterceptor implements Interceptor {
 		try {
 			final Request copy = request.newBuilder().build();
 			final Buffer buffer = new Buffer();
-			copy.body().writeTo(buffer);
-			return buffer.readUtf8();
+			if(copy.body()!=null) {
+				copy.body().writeTo(buffer);
+				return buffer.readUtf8();
+			}
+			else
+			{
+				buffer.close();
+				return "NO BODY";
+			}
 		} catch (final IOException e) {
 			return "did not work";
 		}
 	}
+
 }
