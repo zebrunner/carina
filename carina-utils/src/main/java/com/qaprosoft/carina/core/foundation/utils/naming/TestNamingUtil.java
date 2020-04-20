f46a9afe8e12c271a7e764ae68e7982b352d0618
vdelendik <vadim.delendik@gmail.com>
2020-04-20 23:32:10 +0300

    #986: updated copyright year by 2020

--- carina-utils/src/main/java/com/qaprosoft/carina/core/foundation/utils/naming/TestNamingUtil.java
+++ carina-utils/src/main/java/com/qaprosoft/carina/core/foundation/utils/naming/TestNamingUtil.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright 2013-2019 QaProSoft (http://www.qaprosoft.com).
+ * Copyright 2013-2020 QaProSoft (http://www.qaprosoft.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
