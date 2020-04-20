c40c6bbe31fec8fbcbd8d08f1884b02eeef03709
Nikolai Krasnik <kolya.krasnik@gmail.com>
2020-04-20 18:08:49 +0300

    Added logic to set mobile device locale and language

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/mobile/MobileCapabilies.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/impl/mobile/MobileCapabilies.java
@@ -15,19 +15,51 @@
  *******************************************************************************/
 package com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobile;
 
+import org.apache.log4j.Logger;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import com.qaprosoft.carina.core.foundation.commons.SpecialKeywords;
+import com.qaprosoft.carina.core.foundation.utils.Configuration;
+import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 import com.qaprosoft.carina.core.foundation.webdriver.core.capability.AbstractCapabilities;
 
 public class MobileCapabilies extends AbstractCapabilities {
+    
+    private static final Logger LOGGER = Logger.getLogger(MobileCapabilies.class);
 
     @Override
     public DesiredCapabilities getCapability(String testName) {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
+        //this step should be executed before initCapabiliries() to be able to override this capabilities by default appium approach.
+        capabilities = setLocaleAndLanguage(capabilities);
         // add capabilities based on dynamic _config.properties variables
         capabilities = initCapabilities(capabilities);
         return capabilities;
-    }
+    }    
     
+    private DesiredCapabilities setLocaleAndLanguage(DesiredCapabilities caps) {
+        /*
+         * http://appium.io/docs/en/writing-running-appium/caps/ locale and language
+         * Locale to set for iOS (XCUITest driver only) and Android.
+         * fr_CA format for iOS. CA format (country name abbreviation) for Android
+         */
+        
+        // parse locale param as it has language and country by default like en_US
+        String localeValue = Configuration.get(Parameter.LOCALE);
+        LOGGER.debug("Default locale value is : " + localeValue);
+        String[] values = localeValue.split("_");
+        if (values.length == 2) {
+            if (Configuration.getPlatform().equalsIgnoreCase(SpecialKeywords.ANDROID)) {
+                LOGGER.debug("Put language and locale to android capabilities. language: " + values[0] + "; locale: " + values[1]);
+                caps.setCapability("language", values[0]);
+                caps.setCapability("locale", values[1]);
+            } else if (Configuration.getPlatform().equalsIgnoreCase(SpecialKeywords.IOS)) {
+                LOGGER.debug("Put language and locale to iOS capabilities. language: " + values[0] + "; locale: " + localeValue);
+                caps.setCapability("language", values[0]);
+                caps.setCapability("locale", localeValue);
+            }        
+        }
+        return caps;
+    }
 }
