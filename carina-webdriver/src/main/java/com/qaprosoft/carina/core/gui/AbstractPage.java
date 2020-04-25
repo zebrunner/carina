b6a2303df8e34d34439e3e355520a373d787d16a
vdelendik <vadim.delendik@gmail.com>
2020-04-25 03:08:27 +0300

    removed saveAsPdf

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/AbstractPage.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/AbstractPage.java
@@ -15,24 +15,11 @@
  *******************************************************************************/
 package com.qaprosoft.carina.core.gui;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-
 import org.openqa.selenium.WebDriver;
 
-import com.itextpdf.text.Document;
-import com.itextpdf.text.DocumentException;
-import com.itextpdf.text.Image;
-import com.itextpdf.text.PageSize;
-import com.itextpdf.text.RectangleReadOnly;
-import com.itextpdf.text.pdf.PdfWriter;
-import com.qaprosoft.carina.core.foundation.report.ReportContext;
 import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 import com.qaprosoft.carina.core.foundation.utils.factory.ICustomTypePageFactory;
-import com.qaprosoft.carina.core.foundation.utils.naming.TestNamingUtil;
-import com.qaprosoft.carina.core.foundation.webdriver.Screenshot;
 
 /**
  * All page POJO objects should extend this abstract page to get extra logic.
@@ -79,47 +66,6 @@ public abstract class AbstractPage extends AbstractUIObject implements ICustomTy
 
     public boolean isPageOpened(long timeout) {
         return isPageOpened(this, timeout);
-    }
-
-    public String savePageAsPdf(boolean scaled) throws IOException, DocumentException {
-        String pdfName = "";
-
-        // Define test screenshot root
-        String test = "";
-        if (TestNamingUtil.isTestNameRegistered()) {
-            test = TestNamingUtil.getTestNameByThread();
-        } else {
-            test = "undefined";
-        }
-
-        File testRootDir = ReportContext.getTestDir();
-        File artifactsFolder = ReportContext.getArtifactsFolder();
-
-        String fileID = test.replaceAll("\\W+", "_") + "-" + System.currentTimeMillis();
-        pdfName = fileID + ".pdf";
-
-        String fullPdfPath = artifactsFolder.getAbsolutePath() + "/" + pdfName;
-        // TODO: test this implementation and change back to capture if necessary
-        Image image = Image.getInstance(testRootDir.getAbsolutePath() + "/" + Screenshot.captureFailure(getDriver(), ""));
-        Document document = null;
-        if (scaled) {
-            document = new Document(PageSize.A4, 10, 10, 10, 10);
-            if (image.getHeight() > (document.getPageSize().getHeight() - 20)
-                    || image.getScaledWidth() > (document.getPageSize().getWidth() - 20)) {
-                image.scaleToFit(document.getPageSize().getWidth() - 20, document.getPageSize().getHeight() - 20);
-            }
-        } else {
-            document = new Document(new RectangleReadOnly(image.getScaledWidth(), image.getScaledHeight()));
-        }
-        PdfWriter.getInstance(document, new FileOutputStream(fullPdfPath));
-        document.open();
-        document.add(image);
-        document.close();
-        return fullPdfPath;
-    }
-
-    public String savePageAsPdf() throws IOException, DocumentException {
-        return savePageAsPdf(true);
     }
 
 	private String getUrl() {
