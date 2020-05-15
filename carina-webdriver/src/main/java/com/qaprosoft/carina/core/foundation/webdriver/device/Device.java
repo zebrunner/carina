cb0f9ecfa27e595477cd67871663ec295537e1f9
vdelendik <vadim.delendik@gmail.com>
2020-05-15 11:51:49 +0300

    removed unused imports

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/device/Device.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/device/Device.java
@@ -22,12 +22,6 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
