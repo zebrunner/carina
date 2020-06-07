e2de959b450eff4afca9993e140fa9b8f9914ce5
vdelendik <vadim.delendik@gmail.com>
2020-06-07 17:28:07 +0300

    formatting changes for L10N class

--- carina-utils/src/main/java/com/qaprosoft/carina/core/foundation/utils/resources/L10N.java
+++ carina-utils/src/main/java/com/qaprosoft/carina/core/foundation/utils/resources/L10N.java
@@ -33,17 +33,15 @@ import com.qaprosoft.carina.core.foundation.utils.Configuration;
 import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
 
 /*
- * QUALITY-1076:
  * http://maven.apache.org/surefire/maven-surefire-plugin/examples/class-loading.html
  * Need to set useSystemClassLoader=false for maven surefire plugin to receive access to classloader L10N files on CI
  * <plugin>
  * <groupId>org.apache.maven.plugins</groupId>
  * <artifactId>maven-surefire-plugin</artifactId>
- * <version>2.18.1</version>
+ * <version>3.0.0-M4</version>
  * <configuration>
  * <useSystemClassLoader>false</useSystemClassLoader>
  * </configuration>
- * 
  */
 
 public class L10N {
@@ -60,84 +58,79 @@ public class L10N {
                 .get(Parameter.LOCALE));
 
         List<String> loadedResources = new ArrayList<String>();
+        
+        try {
 
-        for (URL u : Resources.getResourceURLs(new ResourceURLFilter() {
-            public @Override boolean accept(URL u) {
-				String s = u.getPath();
-				boolean contains = s.contains(SpecialKeywords.L10N);
-				if (contains) {
-					LOGGER.debug("L10N: file URL: " + u);
-				}
-				return contains;
-            }
-        })) {
-            LOGGER.debug(String.format(
-                    "Analyzing '%s' L10N resource for loading...", u));
-
-            // workable examples for resource loading are
-            // ResourceBundle.getBundle("L10N.messages", locale);
-            // ResourceBundle.getBundle("L10N.system.data-access.resources.gwt.datasourceAdminDialog",
-            // locale);
-
-            /*
-             * 2. Exclude localization resources like such L10N.messages_de,
-             * L10N.messages_ptBR etc... Note: we ignore valid resources if 3rd
-             * or 5th char from the end is "_". As designed :(
-             */
-            String fileName = FilenameUtils.getBaseName(u.getPath());
-
-            if (u.getPath().endsWith("L10N.class")
-                    || u.getPath().endsWith("L10N$1.class")) {
-                // separate conditions to support core JUnit tests
-                continue;
-            }
-
-            if (fileName.lastIndexOf('_') == fileName.length() - 3
-                    || fileName.lastIndexOf('_') == fileName.length() - 5) {
-                LOGGER.debug(String
-                        .format("'%s' resource IGNORED as it looks like localized resource!",
-                                fileName));
-                continue;
-            }
-            /*
-             * convert "file:
-             * E:\pentaho\qa-automation\target\classes\L10N\messages
-             * .properties" to "L10N.messages"
-             */
-            String filePath = FilenameUtils.getPath(u.getPath());
-            int index = filePath.indexOf(SpecialKeywords.L10N);
-
-            if (index == -1) {
-                LOGGER.warn("Unable to find L10N pattern for " + u.getPath() + " resource!");
-                continue;
-            }
-
-            String resource = filePath.substring(
-                    filePath.indexOf(SpecialKeywords.L10N))
-                    .replaceAll("/", ".")
-                    + fileName;
-
-            if (!loadedResources.contains(resource)) {
-                loadedResources.add(resource);
-                try {
-                    LOGGER.debug(String.format("Adding '%s' resource...",
-                            resource));
-                    for (Locale locale : locales) {
-                        resBoundles.add(ResourceBundle.getBundle(resource, locale));
+            for (URL u : Resources.getResourceURLs(new ResourceURLFilter() {
+                public @Override boolean accept(URL u) {
+                    String s = u.getPath();
+                    boolean contains = s.contains(SpecialKeywords.L10N);
+                    if (contains) {
+                        LOGGER.debug("L10N: file URL: " + u);
                     }
-                    LOGGER.debug(String
-                            .format("Resource '%s' added.", resource));
-                } catch (MissingResourceException e) {
-                    LOGGER.debug(e.getMessage(), e);
+                    return contains;
                 }
-            } else {
-                LOGGER.debug(String
-                        .format("Requested resource '%s' is already loaded into the ResourceBundle!",
-                                resource));
-            }
-        }
+            })) {
+                LOGGER.debug(String.format(
+                        "Analyzing '%s' L10N resource for loading...", u));
+                /*
+                 * 2. Exclude localization resources like such L10N.messages_de, L10N.messages_ptBR etc...
+                 * Note: we ignore valid resources if 3rd or 5th char from the end is "_". As designed :(
+                 */
+                String fileName = FilenameUtils.getBaseName(u.getPath());
 
-        LOGGER.debug("init: L10N bundle size: " + resBoundles.size());
+                if (u.getPath().endsWith("L10N.class")
+                        || u.getPath().endsWith("L10N$1.class")) {
+                    // separate conditions to support core JUnit tests
+                    continue;
+                }
+
+                if (fileName.lastIndexOf('_') == fileName.length() - 3
+                        || fileName.lastIndexOf('_') == fileName.length() - 5) {
+                    LOGGER.debug(String
+                            .format("'%s' resource IGNORED as it looks like localized resource!",
+                                    fileName));
+                    continue;
+                }
+                /*
+                 * convert "file: <REPO>\target\classes\L10N\messages.properties" to "L10N.messages"
+                 */
+                String filePath = FilenameUtils.getPath(u.getPath());
+                int index = filePath.indexOf(SpecialKeywords.L10N);
+
+                if (index == -1) {
+                    LOGGER.warn("Unable to find L10N pattern for " + u.getPath() + " resource!");
+                    continue;
+                }
+
+                String resource = filePath.substring(
+                        filePath.indexOf(SpecialKeywords.L10N))
+                        .replaceAll("/", ".")
+                        + fileName;
+
+                if (!loadedResources.contains(resource)) {
+                    loadedResources.add(resource);
+                    try {
+                        LOGGER.debug(String.format("Adding '%s' resource...",
+                                resource));
+                        for (Locale locale : locales) {
+                            resBoundles.add(ResourceBundle.getBundle(resource, locale));
+                        }
+                        LOGGER.debug(String
+                                .format("Resource '%s' added.", resource));
+                    } catch (MissingResourceException e) {
+                        LOGGER.debug(e.getMessage(), e);
+                    }
+                } else {
+                    LOGGER.debug(String
+                            .format("Requested resource '%s' is already loaded into the ResourceBundle!",
+                                    resource));
+                }
+            }
+            LOGGER.debug("init: L10N bundle size: " + resBoundles.size());
+        } catch (ClassCastException e) {
+            LOGGER.error("Unable to use L10N resources yet using Java 9+!");
+        }
     }
 
     /**
@@ -210,8 +203,7 @@ public class L10N {
     }
 
     /*
-     * QUALITY-1282: This method helps when translating strings that have single
-     * quotes or other special characters that get omitted.
+     * This method helps when translating strings that have single quote or other special characters that get omitted.
      */
     public static String formatString(String resource, String... parameters) {
         for (int i = 0; i < parameters.length; i++) {
@@ -223,7 +215,7 @@ public class L10N {
 
     /*
      * Make sure you remove the single quotes around %s in xpath as string
-     * returned will either have it added for you or single quotes won't be
+     * returned will either have it added for you or single quote won't be
      * added as concat() doesn't need them.
      */
     public static String generateConcatForXPath(String xpathString) {
