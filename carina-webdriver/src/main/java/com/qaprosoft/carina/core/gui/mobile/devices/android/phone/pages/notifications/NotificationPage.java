3aa2c216fe25e956a18cce21d5a38f3c9b36a16f
Brian Suscavage <brian.suscavage@bamtechmedia.com>
2020-09-09 09:55:09 -0400

    Updating outdated clear-all method and adding new click method

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/mobile/devices/android/phone/pages/notifications/NotificationPage.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/mobile/devices/android/phone/pages/notifications/NotificationPage.java
@@ -17,10 +17,13 @@ package com.qaprosoft.carina.core.gui.mobile.devices.android.phone.pages.notific
 
 import java.util.List;
 
+import com.qaprosoft.carina.core.foundation.utils.android.IAndroidUtils;
+import com.qaprosoft.carina.core.foundation.webdriver.DriverHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.apache.log4j.Logger;
@@ -35,7 +38,7 @@ import com.qaprosoft.carina.core.gui.mobile.devices.MobileAbstractPage;
 
 import io.appium.java_client.MobileBy;
 
-public class NotificationPage extends MobileAbstractPage {
+public class NotificationPage extends MobileAbstractPage implements IAndroidUtils{
 
     private static final Logger LOGGER = Logger.getLogger(NotificationPage.class);
 
@@ -63,8 +66,11 @@ public class NotificationPage extends MobileAbstractPage {
     @FindBy(xpath = "//*[@resource-id = 'android:id/status_bar_latest_event_content']/*")
     protected List<ExtendedWebElement> notificationsOtherDevices;
 
-    @FindBy(xpath = "//*[@resource-id='com.android.systemui:id/dismiss_text' " +
-            "or @resource-id='com.android.systemui:id/clear_all_button']")
+    @FindAll({
+            @FindBy(id = "com.android.systemui:id/dismiss_text"),
+            @FindBy(id = "com.android.systemui:id/clear_all_button"),
+            @FindBy(id = "com.android.systemui:id/clear_all")
+    })
     protected ExtendedWebElement dismissBtn;
 
     // Found stable solution
@@ -88,11 +94,14 @@ public class NotificationPage extends MobileAbstractPage {
 
     String itemTitle_Locator_Text = "android:id/title";
 
-    @FindBys({
+    @FindAll({
             @FindBy(id = "android:id/big_text"),
             @FindBy(id = "android:id/text")
     })
-    private List<ExtendedWebElement> itemText;
+    private List<ExtendedWebElement> itemTextList;
+
+    @FindBy(xpath = "//*[@text='%s'")
+    private ExtendedWebElement textItem;
 
     String itemText_Phone_Locator_Text = "android:id/text";
     String itemText_Tablet_Locator_Text = "android:id/big_text";
@@ -198,29 +207,19 @@ public class NotificationPage extends MobileAbstractPage {
             notificationService.expandStatusBar();
         }
         if (dismissBtn.isElementPresent(SHORT_TIMEOUT)) {
-            LOGGER.info("Dismiss all notifications btn is present.");
+            LOGGER.info("'Dismiss All Notifications' btn is present.");
             dismissBtn.click();
-        } else {
-            LOGGER.info("Dismiss all notifications btn isn't present. Attempt to clear all notifications manually");
-            LOGGER.debug("Notifications page source: ".concat(getDriver().getPageSource()));
-            int x1, x2, y1, y2;
-            Point point;
-            Dimension dim;
-            List<ExtendedWebElement> notificationList;
-            if (notifications.size() > 0) {
-                notificationList = notifications;
+        } try{
+            swipe(dismissBtn, notification_scroller);
+            if(dismissBtn.getAttribute("enabled").equals("true")) {
+                LOGGER.info("Clicking 'Dismiss All Notifications' button...");
+                dismissBtn.click();
             } else {
-                notificationList = notificationsOtherDevices;
+                LOGGER.info("'Dismiss All Notifications' Button is present but disabled, meaning any alerts displayed are not closable. Collapsing tray...");
+                pressBack();
             }
-            LOGGER.info("Visible Notifications size:" + notificationList.size());
-            for (ExtendedWebElement notification : notificationList) {
-                point = notification.getElement().getLocation();
-                dim = notification.getElement().getSize();
-                x1 = point.x + dim.width / 6;
-                x2 = point.x + dim.width * 5 / 6;
-                y1 = y2 = point.y + dim.height / 2;
-                MobileUtils.swipe(x1, y1, x2, y2, SWIPE_DURATION);
-            }
+        } catch (AssertionError e){
+            LOGGER.info("Device tray closed by swiping which means no notifications were present. Proceeding with test.");
         }
     }
 
@@ -261,6 +260,11 @@ public class NotificationPage extends MobileAbstractPage {
         return isOpened(DELAY);
     }
 
+    public void clickAlert(String alertText){
+        isStatusBarExpanded();
+        textItem.format(alertText).click();
+    }
+
     /**
      * isOpened
      *
@@ -274,7 +278,7 @@ public class NotificationPage extends MobileAbstractPage {
 
     @Override
     public boolean isOpened() {
-        return isOpened(EXPLICIT_TIMEOUT);
+        return isOpened(DriverHelper.EXPLICIT_TIMEOUT);
     }
 
 }
