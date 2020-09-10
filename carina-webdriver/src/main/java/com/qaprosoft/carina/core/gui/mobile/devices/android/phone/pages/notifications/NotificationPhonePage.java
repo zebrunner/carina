3aa8cab23ec89460cd3cec048dcb23adabce0c15
Brian Suscavage <brian.suscavage@bamtechmedia.com>
2020-09-10 09:17:39 -0400

    Separating Type import to match local framework designs

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/mobile/devices/android/phone/pages/notifications/NotificationPhonePage.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/mobile/devices/android/phone/pages/notifications/NotificationPhonePage.java
@@ -1,10 +1,11 @@
 package com.qaprosoft.carina.core.gui.mobile.devices.android.phone.pages.notifications;
 
 import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType;
+import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType.Type;
 import org.openqa.selenium.WebDriver;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-@DeviceType(pageType = DeviceType.Type.ANDROID_PHONE, parentClass = NotificationPage.class)
+@DeviceType(pageType = Type.ANDROID_PHONE, parentClass = NotificationPage.class)
 public class NotificationPhonePage extends NotificationPage{
     public NotificationPhonePage(WebDriver driver){
         super(driver);
--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/mobile/devices/android/phone/pages/notifications/NotificationTabletPage.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/gui/mobile/devices/android/phone/pages/notifications/NotificationTabletPage.java
@@ -1,10 +1,11 @@
 package com.qaprosoft.carina.core.gui.mobile.devices.android.phone.pages.notifications;
 
 import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType;
+import com.qaprosoft.carina.core.foundation.utils.factory.DeviceType.Type;
 import org.openqa.selenium.WebDriver;
 
 @SuppressWarnings("squid:MaximumInheritanceDepth")
-@DeviceType(pageType = DeviceType.Type.ANDROID_TABLET, parentClass = NotificationPage.class)
+@DeviceType(pageType = Type.ANDROID_TABLET, parentClass = NotificationPage.class)
 public class NotificationTabletPage extends NotificationPage{
     public NotificationTabletPage(WebDriver driver){
         super(driver);
