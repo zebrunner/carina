333874b81ec4c6a38cadf4618aed259ad78d9ec8
dprymudrau <dprymudrau@solvd.com>
2020-09-11 18:15:50 +0300

    #1111: added disabling of enableVNC and Video caps in case of headless mode. Added more logs.

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/AbstractCapabilities.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/capability/AbstractCapabilities.java
@@ -112,11 +112,20 @@ public abstract class AbstractCapabilities {
             capabilities = addChromeOptions(capabilities);
         }
 
-        if (Configuration.getBoolean(Parameter.HEADLESS)
-                && !BrowserType.FIREFOX.equalsIgnoreCase(browser) && !BrowserType.CHROME.equalsIgnoreCase(browser)) {
-            LOGGER.error(String.format("Headless mode isn't supported by %s browser", browser));
+        if (Configuration.getBoolean(Parameter.HEADLESS)) {
+            if (BrowserType.FIREFOX.equalsIgnoreCase(browser)
+                    || BrowserType.CHROME.equalsIgnoreCase(browser)
+                    && Configuration.getDriverType().equalsIgnoreCase(SpecialKeywords.DESKTOP)) {
+                LOGGER.info("Browser will be started in headless mode. VNC and Video will be disabled.");
+                R.CONFIG.put("capabilities.enableVNC", "false");
+                R.CONFIG.put("capabilities.enableVideo", "false");
+                capabilities.setCapability("enableVNC", false);
+                capabilities.setCapability("enableVideo", false);
+            } else {
+                LOGGER.error(String.format("Headless mode isn't supported by %s browser / platform.", browser));
+            }
         }
-        
+
         return capabilities;
     }
 
@@ -243,7 +252,10 @@ public abstract class AbstractCapabilities {
             options.setExperimentalOption("mobileEmulation", mobileEmulation);
         }
 
-        options.setHeadless(Configuration.getBoolean(Parameter.HEADLESS));
+        if (Configuration.getBoolean(Parameter.HEADLESS)
+                && driverType.equals(SpecialKeywords.DESKTOP)) {
+            options.setHeadless(Configuration.getBoolean(Parameter.HEADLESS));
+        }
 
         caps.setCapability(ChromeOptions.CAPABILITY, options);
         return caps;
@@ -279,7 +291,11 @@ public abstract class AbstractCapabilities {
             }
         }
 
-        options.setHeadless(Configuration.getBoolean(Parameter.HEADLESS));
+        String driverType = Configuration.getDriverType();
+        if (Configuration.getBoolean(Parameter.HEADLESS)
+                && driverType.equals(SpecialKeywords.DESKTOP)) {
+            options.setHeadless(Configuration.getBoolean(Parameter.HEADLESS));
+        }
 
         return caps;
     }
