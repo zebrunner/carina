3d47b1e9d596c0043f41e9b9d506ac2a37b10d96
vdelendik <vadim.delendik@gmail.com>
2020-10-03 08:40:09 +0300

    integrate com.zebrunner.agent logger with ability to override it via core_log_level

--- carina-core/src/main/java/com/qaprosoft/carina/core/foundation/listeners/CarinaListener.java
+++ carina-core/src/main/java/com/qaprosoft/carina/core/foundation/listeners/CarinaListener.java
@@ -190,13 +190,7 @@ public class CarinaListener extends AbstractTestListener implements ISuiteListen
                 Enumeration<?> allLoggers = root.getLoggerRepository().getCurrentCategories();
                 while (allLoggers.hasMoreElements()) {
                     Category tmpLogger = (Category) allLoggers.nextElement();
-                    LOGGER.debug("loggerName: " + tmpLogger.getName());
-                    if ("log4j.logger.org.apache.http.wire".equals(tmpLogger.getName())) {
-                        // update this logger to be able to analyse ZafiraClient calls 
-                        LOGGER.info("Updaged logger level for '" + tmpLogger.getName() + "' to "
-                                + Configuration.get(Parameter.CORE_LOG_LEVEL));
-                        tmpLogger.setLevel(Level.toLevel(Configuration.get(Parameter.CORE_LOG_LEVEL)));
-                    }
+                    // LOGGER.debug("loggerName: " + tmpLogger.getName());
                     for (String coreLogPackage : coreLogPackages) {
                         if (tmpLogger.getName().contains(coreLogPackage.trim())) {
                             LOGGER.info("Updaged logger level for '" + tmpLogger.getName() + "' to "
