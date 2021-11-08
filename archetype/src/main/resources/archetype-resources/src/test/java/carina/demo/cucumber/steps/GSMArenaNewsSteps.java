e3405f5353ea98ab82cbd833a07bbf44abee9b01
Dmitry Plyshevsky <eldarian3@gmail.com>
2021-11-08 19:13:54 +0300

    add copyrights for every java file

--- archetype/src/main/resources/archetype-resources/src/test/java/carina/demo/cucumber/steps/GSMArenaNewsSteps.java
+++ archetype/src/main/resources/archetype-resources/src/test/java/carina/demo/cucumber/steps/GSMArenaNewsSteps.java
@@ -1,8 +1,8 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-/*
- * Copyright 2013-2021 QAPROSOFT (http://qaprosoft.com/).
+/*******************************************************************************
+ * Copyright 2018-2022 Zebrunner Inc (https://www.zebrunner.com).
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -15,7 +15,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *******************************************************************************/
 package ${package}.carina.demo.cucumber.steps;
 
 
