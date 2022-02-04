e862f2afbedb6e760b08dd7e251d7d8a315cc173
Andrei Kamarouski <akamarouski@Andreis-MacBook-Pro-3.local>
2022-02-04 20:01:15 +0300

    refactor code

--- carina-api/src/test/java/com/qaprosoft/carina/core/foundation/api/annotation/ApiMethodAnnotationTest.java
+++ carina-api/src/test/java/com/qaprosoft/carina/core/foundation/api/annotation/ApiMethodAnnotationTest.java
@@ -15,28 +15,26 @@
  *******************************************************************************/
 package com.qaprosoft.carina.core.foundation.api.annotation;
 
-import com.qaprosoft.carina.core.foundation.api.http.ContentTypeEnum;
-import io.restassured.internal.RequestSpecificationImpl;
 import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class ApiMethodAnnotationTest {
-    private ApiMethodWAnnotation m;
+import com.qaprosoft.carina.core.foundation.api.http.ContentTypeEnum;
 
-    @BeforeClass
-    public void init() {
-        m = new ApiMethodWAnnotation();
-    }
+import io.restassured.internal.RequestSpecificationImpl;
+
+public class ApiMethodAnnotationTest {
 
     @Test
     public void testEndpoint() {
+        ApiMethodWAnnotation m = new ApiMethodWAnnotation();
         Assert.assertEquals(m.getMethodPath(), "http://test.api.com", "Method path from annotation not as expected");
     }
 
     @Test
     public void testContentType() {
+        ApiMethodWAnnotation m = new ApiMethodWAnnotation();
         Assert.assertEquals(((RequestSpecificationImpl) m.getRequest()).getContentType(), ContentTypeEnum.XML
                 .getStringValues()[0], "Content type from annotation not as expected");
     }
+
 }
