b38c707961e16b5fbfd2415d9f35ba01dd92a1f8
Andrei Kamarouski <akamarouski@solvd.com>
2022-04-28 10:16:41 +0300

    Added doc comment for CaseInsensitiveXPath annotation

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/decorator/annotations/CaseInsensitiveXPath.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/decorator/annotations/CaseInsensitiveXPath.java
@@ -20,15 +20,44 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation allows converting specific parts of the xpath locator to case-insensitive.
+ * Applies to the following types of locators: id, name, xpath, linkText.
+ * For all types of locators, except for xpath, before converting to case-insensitive, there is
+ * a conversion to xpath.
+ * The logic of work for web and mobile is different (the presence or absence of a link to the mobile application in
+ * Carina's configuration file depends on what logic is performed).
+ *
+ * You can choose, which part of the locator will be transformed (id, name, text, class).
+ *
+ * When you convert linkText, there are text() attribute appear, so if you want to convert linkText, you should
+ * set text as true
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.FIELD, ElementType.TYPE })
 public @interface CaseInsensitiveXPath {
 
+    /**
+     * web: @id
+     * mobile: @resource-id
+     */
     boolean id() default false;
 
+    /**
+     * web: @name
+     * mobile: @name
+     */
     boolean name() default false;
 
+    /**
+     * web: text()
+     * mobile: @text, text(), @content-desc
+     */
     boolean text() default true;
 
+    /**
+     * web: @class
+     * mobile: @class
+     */
     boolean classAttr() default false;
 }
