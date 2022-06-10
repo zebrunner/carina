44351cc12a24cb7a8853b6d2ffbb657393ca3dc5
Andrei Kamarouski <akamarouski@solvd.com>
2022-06-10 10:57:11 +0300

    Changed waiting to getDefaultCondition

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/decorator/ExtendedWebElement.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/decorator/ExtendedWebElement.java
@@ -1129,27 +1129,13 @@ public class ExtendedWebElement implements IWebElement {
 
     public boolean waitUntilElementDisappear(final long timeout) {
     	try {
-        	//TODO: investigate maybe searchContext better to use here!
     		//do direct selenium/appium search without any extra validations
-            if (searchContext != null) {
-                //TODO: use-case when format method is used. Need investigate howto init context in this case as well
-                element = searchContext.findElement(by);
-            } else {
-                LOGGER.debug("waitUntilElementDisappear: searchContext is null for " + getNameWithLocator());
-                element = getDriver().findElement(by);  
-            }
+            element = searchContext.findElement(by);
     	} catch (NoSuchElementException e) {
     		//element not present so means disappear
     		return true;
-    	} catch (Exception e) {
-    		//element not present so means disappear
-    		LOGGER.error("Investigate use-case with disappeared element later!", e);
-    		return true;
     	}
-
-        return waitUntil(ExpectedConditions.or(ExpectedConditions.invisibilityOfElementLocated(getBy()),
-                ExpectedConditions.stalenessOf(element),
-                ExpectedConditions.invisibilityOf(element)), timeout);
+        return waitUntil(ExpectedConditions.not(getDefaultCondition(getBy())), timeout);
     }
 
     public ExtendedWebElement format(Object... objects) {
