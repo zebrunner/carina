1e97c126f8a3c0bad9545e41df8229d33c833c51
Andrei Kamarouski <akamarouski@solvd.com>
2022-07-28 16:15:11 +0300

    Updated from AppiumCommandExecutor class changes

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/EventFiringAppiumCommandExecutor.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/EventFiringAppiumCommandExecutor.java
@@ -17,6 +17,7 @@ package com.qaprosoft.carina.core.foundation.webdriver.listener;
 
 import static com.google.common.base.Preconditions.checkNotNull;
 import static java.util.Optional.ofNullable;
+import static org.openqa.selenium.remote.DriverCommand.NEW_SESSION;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
@@ -24,31 +25,32 @@ import java.net.ConnectException;
 import java.net.URL;
 import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
+import org.openqa.selenium.SessionNotCreatedException;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.remote.Command;
 import org.openqa.selenium.remote.CommandCodec;
+import org.openqa.selenium.remote.CommandExecutor;
 import org.openqa.selenium.remote.CommandInfo;
+import org.openqa.selenium.remote.Dialect;
 import org.openqa.selenium.remote.DriverCommand;
 import org.openqa.selenium.remote.HttpCommandExecutor;
+import org.openqa.selenium.remote.ProtocolHandshake;
 import org.openqa.selenium.remote.Response;
+import org.openqa.selenium.remote.ResponseCodec;
+import org.openqa.selenium.remote.codec.w3c.W3CHttpCommandCodec;
 import org.openqa.selenium.remote.http.HttpClient;
 import org.openqa.selenium.remote.http.HttpRequest;
 import org.openqa.selenium.remote.http.HttpResponse;
-//import org.openqa.selenium.remote.http.W3CHttpCommandCodec;
-import org.openqa.selenium.remote.codec.w3c.W3CHttpCommandCodec;
 import org.openqa.selenium.remote.service.DriverService;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Throwables;
-import com.qaprosoft.carina.core.foundation.commons.SpecialKeywords;
-import com.qaprosoft.carina.core.foundation.utils.Configuration;
-import com.qaprosoft.carina.core.foundation.utils.Configuration.Parameter;
-import com.qaprosoft.carina.core.foundation.utils.common.CommonUtils;
-//import com.qaprosoft.carina.core.foundation.webdriver.httpclient.HttpClientFactoryCustom;
 
 import io.appium.java_client.MobileCommand;
 import io.appium.java_client.remote.AppiumCommandExecutor;
+import io.appium.java_client.remote.AppiumProtocolHandshake;
 import io.appium.java_client.remote.AppiumW3CHttpCommandCodec;
 
 //import io.appium.java_client.remote.NewAppiumSessionPayload;
@@ -62,15 +64,13 @@ import io.appium.java_client.remote.AppiumW3CHttpCommandCodec;
  */
 @SuppressWarnings({ "unchecked" })
 public class EventFiringAppiumCommandExecutor extends HttpCommandExecutor {
-    //private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final String IDEMPOTENCY_KEY_HEADER = "X-Idempotency-Key";
 
     private final Optional<DriverService> serviceOptional;
 
-    private EventFiringAppiumCommandExecutor(Map<String, CommandInfo> additionalCommands, DriverService service,
-            URL addressOfRemoteServer,
+    private EventFiringAppiumCommandExecutor(Map<String, CommandInfo> additionalCommands, DriverService service, URL addressOfRemoteServer,
             HttpClient.Factory httpClientFactory) {
-        super(additionalCommands,
-                ofNullable(service)
+        super(additionalCommands, ofNullable(service)
                         .map(DriverService::getUrl)
                         .orElse(addressOfRemoteServer),
                 httpClientFactory);
@@ -87,25 +87,25 @@ public class EventFiringAppiumCommandExecutor extends HttpCommandExecutor {
         this(additionalCommands, null, checkNotNull(addressOfRemoteServer), httpClientFactory);
     }
 
-    //TODO import latest java-appium client
-/*    public EventFiringAppiumCommandExecutor(Map<String, CommandInfo> additionalCommands,
+    public EventFiringAppiumCommandExecutor(Map<String, CommandInfo> additionalCommands,
             URL addressOfRemoteServer) {
-        this(additionalCommands, addressOfRemoteServer, new HttpClientFactoryCustom());
-    }*/
+        this(additionalCommands, addressOfRemoteServer, HttpClient.Factory.createDefault());
+    }
 
-/*    public EventFiringAppiumCommandExecutor(Map<String, CommandInfo> additionalCommands,
+    public EventFiringAppiumCommandExecutor(Map<String, CommandInfo> additionalCommands,
             DriverService service) {
-        this(additionalCommands, service, new HttpClientFactoryCustom());
-    }*/
+        this(additionalCommands, service, HttpClient.Factory.createDefault());
+    }
 
     public EventFiringAppiumCommandExecutor(URL addressOfRemoteServer) {
     	this(MobileCommand.commandRepository, addressOfRemoteServer, HttpClient.Factory.createDefault());
-        //this(MobileCommand.commandRepository, addressOfRemoteServer, new HttpClientFactoryCustom());
     }
 
-    private <B> B getPrivateFieldValue(String fieldName, Class<B> fieldType) {
+    @SuppressWarnings("SameParameterValue")
+    private <B> B getPrivateFieldValue(
+            Class<? extends CommandExecutor> cls, String fieldName, Class<B> fieldType) {
         try {
-            final Field f = getClass().getSuperclass().getDeclaredField(fieldName);
+            final Field f = cls.getDeclaredField(fieldName);
             f.setAccessible(true);
             return fieldType.cast(f.get(this));
         } catch (NoSuchFieldException | IllegalAccessException e) {
@@ -113,9 +113,11 @@ public class EventFiringAppiumCommandExecutor extends HttpCommandExecutor {
         }
     }
 
-    private void setPrivateFieldValue(String fieldName, Object newValue) {
+    @SuppressWarnings("SameParameterValue")
+    private void setPrivateFieldValue(
+            Class<? extends CommandExecutor> cls, String fieldName, Object newValue) {
         try {
-            final Field f = getClass().getSuperclass().getDeclaredField(fieldName);
+            final Field f = cls.getDeclaredField(fieldName);
             f.setAccessible(true);
             f.set(this, newValue);
         } catch (NoSuchFieldException | IllegalAccessException e) {
@@ -125,16 +127,49 @@ public class EventFiringAppiumCommandExecutor extends HttpCommandExecutor {
 
     private Map<String, CommandInfo> getAdditionalCommands() {
         // noinspection unchecked
-        return getPrivateFieldValue("additionalCommands", Map.class);
+        return getPrivateFieldValue(HttpCommandExecutor.class, "additionalCommands", Map.class);
     }
 
     private CommandCodec<HttpRequest> getCommandCodec() {
         // noinspection unchecked
-        return getPrivateFieldValue("commandCodec", CommandCodec.class);
+        return getPrivateFieldValue(HttpCommandExecutor.class, "commandCodec", CommandCodec.class);
     }
 
     private void setCommandCodec(CommandCodec<HttpRequest> newCodec) {
-        setPrivateFieldValue("commandCodec", newCodec);
+        setPrivateFieldValue(HttpCommandExecutor.class, "commandCodec", newCodec);
+    }
+
+    protected void setResponseCodec(ResponseCodec<HttpResponse> codec) {
+        setPrivateFieldValue(HttpCommandExecutor.class, "responseCodec", codec);
+    }
+
+    protected HttpClient getClient() {
+        return getPrivateFieldValue(HttpCommandExecutor.class, "client", HttpClient.class);
+    }
+
+    private Response createSession(Command command) throws IOException {
+        if (getCommandCodec() != null) {
+            throw new SessionNotCreatedException("Session already exists");
+        }
+
+        ProtocolHandshake.Result result = new AppiumProtocolHandshake().createSession(
+                getClient().with((httpHandler) -> (req) -> {
+                    req.setHeader(IDEMPOTENCY_KEY_HEADER, UUID.randomUUID().toString().toLowerCase());
+                    return httpHandler.execute(req);
+                }), command);
+        Dialect dialect = result.getDialect();
+        if (!(dialect.getCommandCodec() instanceof W3CHttpCommandCodec)) {
+            throw new SessionNotCreatedException("Only W3C sessions are supported. "
+                    + "Please make sure your server is up to date.");
+        }
+        setCommandCodec(new AppiumW3CHttpCommandCodec());
+        refreshAdditionalCommands();
+        setResponseCodec(dialect.getResponseCodec());
+        return result.createResponse();
+    }
+
+    public void refreshAdditionalCommands() {
+        getAdditionalCommands().forEach(this::defineCommand);
     }
 
     @Override
@@ -144,14 +179,14 @@ public class EventFiringAppiumCommandExecutor extends HttpCommandExecutor {
                 try {
                     driverService.start();
                 } catch (IOException e) {
-                    throw new WebDriverException(e);
+                    throw new WebDriverException(e.getMessage(), e);
                 }
             });
         }
 
         Response response = null;
         try {
-            response = super.execute(command);
+            return NEW_SESSION.equals(command.getName()) ? createSession(command) : super.execute(command);
         } catch (Throwable t) {
             Throwable rootCause = Throwables.getRootCause(t);
             if (rootCause instanceof ConnectException
@@ -172,16 +207,6 @@ public class EventFiringAppiumCommandExecutor extends HttpCommandExecutor {
                 serviceOptional.ifPresent(DriverService::stop);
             }
         }
-
-        //TODO [MS] this issue solved with appium java-client 8.0 +
-/*        if (DriverCommand.NEW_SESSION.equals(command.getName())
-                && getCommandCodec() instanceof W3CHttpCommandCodec) {
-            setCommandCodec(new AppiumW3CHttpCommandCodec());
-            getAdditionalCommands().forEach(this::defineCommand);
-        }*/
-
-
-        return response;
     }
 
 }
