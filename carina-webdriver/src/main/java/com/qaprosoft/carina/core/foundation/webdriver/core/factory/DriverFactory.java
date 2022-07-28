81262bb172fab0aa5fc497bd5e4b381a648f328e
Andrei Kamarouski <akamarouski@solvd.com>
2022-07-28 11:12:32 +0300

    Commented stf factory choosing

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/DriverFactory.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/DriverFactory.java
@@ -38,8 +38,6 @@ import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.CustomIO
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.EdgeFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.FirefoxFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.IOSFactory;
-import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.STFAndroidFactory;
-import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.STFIOSFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.SafariFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.impl.WindowsFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.listener.DriverListener;
@@ -105,13 +103,14 @@ public class DriverFactory {
 
     private static AbstractFactory chooseDriverFactory(Capabilities capabilities) {
 
-        if (STFAndroidFactory.isSuitable(capabilities)) {
-            return new STFAndroidFactory();
-        }
-
-        if (STFIOSFactory.isSuitable(capabilities)) {
-            return new STFIOSFactory();
-        }
+        //experiment over
+        // if (STFAndroidFactory.isSuitable(capabilities)) {
+        // return new STFAndroidFactory();
+        // }
+        //
+        // if (STFIOSFactory.isSuitable(capabilities)) {
+        // return new STFIOSFactory();
+        // }
 
         if (AndroidFactory.isSuitable(capabilities)) {
             return new AndroidFactory();
