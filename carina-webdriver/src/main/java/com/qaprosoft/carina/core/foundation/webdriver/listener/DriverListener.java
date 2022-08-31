9b166c07651d8100a7b2155e87b6d4324a0ece80
Andrei Kamarouski <akamarouski@solvd.com>
2022-08-31 11:32:36 +0300

    Commented exception listening in DriverListener

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/DriverListener.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/listener/DriverListener.java
@@ -105,85 +105,85 @@ public class DriverListener implements WebDriverListener, IDriverPool {
         onAfterAction(comment, this.driver);
 
     }
-
-    // TODO investigate this method is too complex, and it contails old logic. Can we simplify it?
-    @Override
-    public void onError(Object target, Method method, Object[] args, InvocationTargetException e) {
-
-        // [VD] make below code as much safety as possible otherwise potential recursive failure could occur with driver related issue.
-        // most suspicious are capture screenshots, generating dumps etc
-        if (e == null
-                || e.getMessage() == null
-                || e.getMessage().contains("Method has not yet been implemented")
-                || e.getMessage().contains("Expected to read a START_MAP but instead have: END. Last 0 characters read")
-                || e.getMessage().contains("Unable to determine type from: <. Last 1 characters read")
-                || e.getMessage().contains("script timeout")
-                || e.getMessage().contains("javascript error: Cannot read property 'outerHTML' of null")
-                || e.getMessage().contains("javascript error: Cannot read property 'scrollHeight' of null")
-                || e.getMessage().contains("Method is not implemented")
-                || e.getMessage().contains("An element could not be located on the page using the given search parameters")
-                || e.getMessage().contains("no such element: Unable to locate element")
-                || e.getMessage().contains("Failed to execute command screen image")
-                // carina has a lot of extra verifications to solve all stale reference issue and finally perform an action so ignore such exception
-                // in listener!
-                || e.getMessage().contains("StaleElementReferenceException")
-                || e.getMessage().contains("stale_element_reference.html")) {
-            // do nothing
-            return;
-        }
-
-        // handle use-case when application crashed on iOS but tests continue to execute something because doesn't raise valid exception
-        // Example:
-
-        // 10:25:20 2018-09-14 10:29:39 DriverListener [TestNG-31] [ERROR]
-        // [iPhone_6s] An unknown server-side error occurred while
-        // processing the command. Original error: The application under
-        // test with bundle id 'Q5AWL8WCY6' is not running,
-        // possibly crashed (WARNING: The server did not provide any
-        // stacktrace information)
-
-        // TODO: investigate if we run @AfterMethod etc system events after this crash
-        if (e.getMessage().contains("is not running, possibly crashed")) {
-            throw new RuntimeException(e);
-        }
-
-        // hopefully castDriver below resolve root cause of the recursive onException calls but keep below to ensure
-        if (e.getStackTrace() != null
-                && (Arrays.toString(e.getStackTrace())
-                        .contains("com.qaprosoft.carina.core.foundation.webdriver.listener.DriverListener.onException")
-                        || Arrays.toString(e.getStackTrace()).contains("Unable to capture screenshot due to the WebDriverException"))) {
-            LOGGER.error("Do not generate screenshot for invalid driver!");
-            // prevent recursive crash for onException
-            return;
-        }
-
-        LOGGER.debug("DriverListener->onException starting..." + e.getMessage());
-        //this.driver = castDriver(this.driver);
-
-        try {
-            // 1. if you see mess with afterTest carina actions and Timer startup failure you should follow steps #2+ to determine root cause.
-            // Driver initialization 'default' FAILED! Retry 1 of 1 time - Operation already started: mobile_driverdefault
-            // 2. carefully track all preliminary exception for the same thread to detect 1st problematic exception
-            // 3. 99% those root exception means that we should prohibit screenshot generation for such use-case
-            // 4. if 3rd one is true just update Screenshot.isCaptured() adding part of the exception to the list
-            // handle cases which should't be captured
-            if (Screenshot.isCaptured(e.getMessage())) {
-                captureScreenshot(e.getMessage(), driver, null, true);
-            }
-        } catch (Exception err) {
-            if (!err.getMessage().isEmpty()
-                    && (err.getMessage().contains("Method has not yet been implemented")
-                            || (err.getMessage().contains("Method is not implemented")))) {
-                LOGGER.debug("Unrecognized exception detected in DriverListener->onException!", err);
-            } else {
-                LOGGER.error("Unrecognized exception detected in DriverListener->onException!", err);
-            }
-        } catch (Throwable err) {
-            LOGGER.error("Take a look to the logs above for current thread and add exception into the exclusion for Screenshot.isCaptured().", err);
-        }
-
-        LOGGER.debug("DriverListener->onException finished.");
-    }
+//
+//    // TODO investigate this method is too complex, and it contails old logic. Can we simplify it?
+//    @Override
+//    public void onError(Object target, Method method, Object[] args, InvocationTargetException e) {
+//
+//        // [VD] make below code as much safety as possible otherwise potential recursive failure could occur with driver related issue.
+//        // most suspicious are capture screenshots, generating dumps etc
+//        if (e == null
+//                || e.getMessage() == null
+//                || e.getMessage().contains("Method has not yet been implemented")
+//                || e.getMessage().contains("Expected to read a START_MAP but instead have: END. Last 0 characters read")
+//                || e.getMessage().contains("Unable to determine type from: <. Last 1 characters read")
+//                || e.getMessage().contains("script timeout")
+//                || e.getMessage().contains("javascript error: Cannot read property 'outerHTML' of null")
+//                || e.getMessage().contains("javascript error: Cannot read property 'scrollHeight' of null")
+//                || e.getMessage().contains("Method is not implemented")
+//                || e.getMessage().contains("An element could not be located on the page using the given search parameters")
+//                || e.getMessage().contains("no such element: Unable to locate element")
+//                || e.getMessage().contains("Failed to execute command screen image")
+//                // carina has a lot of extra verifications to solve all stale reference issue and finally perform an action so ignore such exception
+//                // in listener!
+//                || e.getMessage().contains("StaleElementReferenceException")
+//                || e.getMessage().contains("stale_element_reference.html")) {
+//            // do nothing
+//            return;
+//        }
+//
+//        // handle use-case when application crashed on iOS but tests continue to execute something because doesn't raise valid exception
+//        // Example:
+//
+//        // 10:25:20 2018-09-14 10:29:39 DriverListener [TestNG-31] [ERROR]
+//        // [iPhone_6s] An unknown server-side error occurred while
+//        // processing the command. Original error: The application under
+//        // test with bundle id 'Q5AWL8WCY6' is not running,
+//        // possibly crashed (WARNING: The server did not provide any
+//        // stacktrace information)
+//
+//        // TODO: investigate if we run @AfterMethod etc system events after this crash
+//        if (e.getMessage().contains("is not running, possibly crashed")) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        // hopefully castDriver below resolve root cause of the recursive onException calls but keep below to ensure
+//        if (e.getStackTrace() != null
+//                && (Arrays.toString(e.getStackTrace())
+//                        .contains("com.qaprosoft.carina.core.foundation.webdriver.listener.DriverListener.onException")
+//                        || Arrays.toString(e.getStackTrace()).contains("Unable to capture screenshot due to the WebDriverException"))) {
+//            LOGGER.error("Do not generate screenshot for invalid driver!");
+//            // prevent recursive crash for onException
+//            return;
+//        }
+//
+//        LOGGER.debug("DriverListener->onException starting..." + e.getMessage());
+//        //this.driver = castDriver(this.driver);
+//
+//        try {
+//            // 1. if you see mess with afterTest carina actions and Timer startup failure you should follow steps #2+ to determine root cause.
+//            // Driver initialization 'default' FAILED! Retry 1 of 1 time - Operation already started: mobile_driverdefault
+//            // 2. carefully track all preliminary exception for the same thread to detect 1st problematic exception
+//            // 3. 99% those root exception means that we should prohibit screenshot generation for such use-case
+//            // 4. if 3rd one is true just update Screenshot.isCaptured() adding part of the exception to the list
+//            // handle cases which should't be captured
+//            if (Screenshot.isCaptured(e.getMessage())) {
+//                captureScreenshot(e.getMessage(), driver, null, true);
+//            }
+//        } catch (Exception err) {
+//            if (!err.getMessage().isEmpty()
+//                    && (err.getMessage().contains("Method has not yet been implemented")
+//                            || (err.getMessage().contains("Method is not implemented")))) {
+//                LOGGER.debug("Unrecognized exception detected in DriverListener->onException!", err);
+//            } else {
+//                LOGGER.error("Unrecognized exception detected in DriverListener->onException!", err);
+//            }
+//        } catch (Throwable err) {
+//            LOGGER.error("Take a look to the logs above for current thread and add exception into the exclusion for Screenshot.isCaptured().", err);
+//        }
+//
+//        LOGGER.debug("DriverListener->onException finished.");
+//    }
 
     /**
      * Converts char sequence to string.
