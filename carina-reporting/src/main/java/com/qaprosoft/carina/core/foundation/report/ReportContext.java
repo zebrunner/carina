7b44752aa7914cf204749688e24580b951bb4c66
Andrei Kamarouski <akamarouski@solvd.com>
2022-11-03 15:28:12 +0300

    refactor(carina-utils): move log folder(ThreadLogAppender) to the carina-reporting

--- carina-reporting/src/main/java/com/qaprosoft/carina/core/foundation/report/ReportContext.java
+++ carina-reporting/src/main/java/com/qaprosoft/carina/core/foundation/report/ReportContext.java
@@ -67,7 +67,7 @@ import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 
 import com.zebrunner.carina.utils.common.SpecialKeywords;
-import com.zebrunner.carina.utils.log.ThreadLogAppender;
+import com.qaprosoft.carina.core.foundation.report.log.ThreadLogAppender;
 import com.zebrunner.carina.utils.Configuration;
 import com.zebrunner.carina.utils.Configuration.Parameter;
 import com.zebrunner.carina.utils.ZipManager;
