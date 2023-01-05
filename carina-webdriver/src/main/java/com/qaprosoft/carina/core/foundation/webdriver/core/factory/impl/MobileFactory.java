844497f700a8f7179f3b4b8a71ef3afe97175705
Andrei Kamarouski <akamarouski@solvd.com>
2023-01-05 13:15:23 +0300

    feature(MobileFactory.java): add static method to MobileFactory to get direct link to the mobile app

--- carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
+++ carina-webdriver/src/main/java/com/qaprosoft/carina/core/foundation/webdriver/core/factory/impl/MobileFactory.java
@@ -24,9 +24,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import com.zebrunner.agent.core.registrar.Artifact;
-import com.zebrunner.carina.commons.artifact.IArtifactManager;
-import com.zebrunner.carina.utils.mobile.ArtifactProvider;
 import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.WebDriver;
@@ -34,10 +31,6 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.zebrunner.carina.utils.commons.SpecialKeywords;
-import com.zebrunner.carina.utils.Configuration;
-import com.zebrunner.carina.utils.Configuration.Parameter;
-import com.zebrunner.carina.utils.R;
 import com.qaprosoft.carina.core.foundation.webdriver.IDriverPool;
 import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobile.EspressoCapabilities;
 import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobile.UiAutomator2Capabilities;
@@ -45,6 +38,13 @@ import com.qaprosoft.carina.core.foundation.webdriver.core.capability.impl.mobil
 import com.qaprosoft.carina.core.foundation.webdriver.core.factory.AbstractFactory;
 import com.qaprosoft.carina.core.foundation.webdriver.device.Device;
 import com.qaprosoft.carina.core.foundation.webdriver.listener.EventFiringAppiumCommandExecutor;
+import com.zebrunner.agent.core.registrar.Artifact;
+import com.zebrunner.carina.commons.artifact.IArtifactManager;
+import com.zebrunner.carina.utils.Configuration;
+import com.zebrunner.carina.utils.Configuration.Parameter;
+import com.zebrunner.carina.utils.R;
+import com.zebrunner.carina.utils.commons.SpecialKeywords;
+import com.zebrunner.carina.utils.mobile.ArtifactProvider;
 
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.ios.IOSDriver;
@@ -60,6 +60,28 @@ import io.appium.java_client.remote.options.SupportsAutomationNameOption;
 public class MobileFactory extends AbstractFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     private static final Map<String, String> CACHE_MOBILE_APP_LINKS = new ConcurrentHashMap<>();
+
+    /**
+     * Get a direct (pre-sign) link to the application.
+     * If the passed link was previously accessed to get a direct link,
+     * it will be taken from the cache, otherwise it will be generated and cached.
+     *
+     * @param originalAppLink link of which a direct link will be made
+     * @return direct link to the mobile application
+     */
+    public static String getAppLink(String originalAppLink) {
+        String updatedMobileApp;
+        if (!CACHE_MOBILE_APP_LINKS.containsKey(originalAppLink)) {
+            IArtifactManager artifactProvider = ArtifactProvider.getInstance();
+            updatedMobileApp = artifactProvider.getDirectLink(originalAppLink);
+            CACHE_MOBILE_APP_LINKS.put(originalAppLink, updatedMobileApp);
+            LOGGER.debug("For the 'app' capability with current value '{}', will be cached link: {}", originalAppLink, updatedMobileApp);
+        } else {
+            updatedMobileApp = CACHE_MOBILE_APP_LINKS.get(originalAppLink);
+            LOGGER.debug("Original value of capability 'app': '{}' will be replaced by cached link: {}", originalAppLink, updatedMobileApp);
+        }
+        return updatedMobileApp;
+    }
 
     @Override
     public WebDriver create(String name, MutableCapabilities capabilities, String seleniumHost) {
