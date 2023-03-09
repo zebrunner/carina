0260eb2ca872f8d9bf3edd87a80c5b9fad9644d6
Andrei Kamarouski <akamarouski@solvd.com>
2023-03-09 14:01:50 +0300

    refactor: move the carina-api module as a separate project

--- carina-api/src/test/java/com/zebrunner/carina/api/mock/apimethod/NoPlaceholdersInURLMethod.java
+++
@@ -1,9 +0,0 @@
-package com.zebrunner.carina.api.mock.apimethod;
-
-import com.zebrunner.carina.api.AbstractApiMethodV2;
-
-public class NoPlaceholdersInURLMethod extends AbstractApiMethodV2 {
-    public NoPlaceholdersInURLMethod() {
-        super();
-    }
-}
